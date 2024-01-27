85308ce74f68edd76035c309e49553b22df90198
ThreeCrown <st3phanadams7@gmail.com>
2024-01-26 21:28:15 -0800

    Ini AppBar Refactor Dependencies
    +VL  Logo edits into Dir in Assets for ./client/
    +viteGlob plugin included
     *needed guidance on plugin compatability

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+import viteGlob from 'vite-plugin-glob';
 
 const appManifest: Partial<VitePWAOptions> = {};
 
 export default defineConfig({
   root: './',
-  plugins: [react({ tsDecorators: true }), VitePWA(appManifest)],
+  plugins: [react({ tsDecorators: true }), VitePWA(appManifest), viteGlob()],
   server: {
     port: 3030,
   },
