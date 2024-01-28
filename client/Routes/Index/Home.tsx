6e72682a33d482b9b3dfbeb6e79040d6bd832d2b
ThreeCrown <st3phanadams7@gmail.com>
2024-01-28 04:09:05 -0800

    HomeNavButton Style Refactor
    Need to fix the Transition on the text still.
    Fixed Style of HomeNavButtons
    Added Transition for the Box

--- client/Routes/Index/Home.tsx
+++ client/Routes/Index/Home.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { HomeNavButton } from './HomeNavButton';
+import { useState } from 'react';
+import { TransitionGroup } from 'react-transition-group';
 
 export const Home: React.FC<unknown> = () => {
+
+  /*const [showFirst, setShowFirst] = useState(true);
+  const [showSecond, setShowSecond] = useState(true);
+  const [showThird, setShowThird] = useState(true);
+  const [showFourth, setShowFourth] = useState(true);
+
+  const toggle = () => {
+    setShowFirst(prev => !prev);
+    setTimeout(() => {
+      setShowSecond(prev => !prev);
+      setShowThird(prev => !prev);
+      setShowFourth(prev => !prev);
+    }, 500); //wait 500ms
+  };*/
+
+  const [isTransitioning, setIsTransitioning] = useState(false);
+  const toggle = () => {
+    if (isTransitioning) return;
+    setIsTransitioning(true);
+
+    setTimeout(() => {
+      setIsTransitioning(false);
+    }, 5000);
+  };
+
   return (
-    <Box marginTop={'3em'}>
-      <HomeNavButton title="Contract Ledger" to="/ledger/contract" />
-      <HomeNavButton title="Personal Ledger" to="/ledger/personal" />
-      <HomeNavButton title="Org Ledger" to="/ledger/org" />
+    <Box marginTop={'5em'} onMouseEnter={toggle} onMouseLeave={toggle}>
+      <TransitionGroup>
+        <HomeNavButton title="Contract Ledger" to="/ledger/contract" />
+        <HomeNavButton title="Verse Market" to="/ledger/versemarket" />
+        <HomeNavButton title="Personal Ledger" to="/ledger/personal" />
+        <HomeNavButton title="Org Ledger" to="/ledger/org" />
+      </TransitionGroup>
     </Box>
   );
 };
--- client/Routes/Index/HomeNavButton.tsx
+++ client/Routes/Index/HomeNavButton.tsx
@@ -12,17 +12,45 @@ export const HomeNavButton: React.FC<HomeNavButtonProps> = (props) => {
   const theme = useTheme() as any;
   return (
     <Box
-      width={'15em'}
-      height={'5em'}
-      marginTop="1em"
-      sx={{ backgroundColor: alpha(theme.palette.primary.main, 0.5) }}
-      display="flex"
+      sx={{
+        backgroundColor: alpha(theme.palette.primary.main, 0.5),
+        borderRadius: '.5em',
+        padding: '.5em',
+        width: '16em',
+        height: '6em',
+        marginTop: '1em',
+        marginLeft: '.5em',
+        display: 'flex',
+        flexGrow: 1,
+        transition: 'width .4s ease-in, height.4s ease-in',
+        transitionDelay: '300ms',
+        '&:hover': {
+          width: '20em',
+          height: '10em',
+        },
+        '&:hover > .nav-button-text': {
+          fontWeight: '800',
+          color: theme.palette.text.secondary,
+          fontSize: '1.2em',
+        }
+      }}
     >
       <Typography
-        variant="body1"
-        alignSelf="flex-end"
-        marginLeft="5em"
-        marginBottom="1em"
+        className='nav-button-text'
+        variant='body1'
+        sx={{
+          color: theme.palette.text,
+          fontWeight: '500',
+          fontSize: '1em',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '.8em',
+          marginLeft: 'auto',
+          marginTop: 'auto',
+          alignSelf: 'flex-end',
+          transition: 'color 300ms ease-in-quad, fontSize 300ms ease-in-quad, fontWeight 300ms ease-in-quad',
+        }}
       >
         {props.title}
       </Typography>
