7a70f1b3291bee96944770282e874811ddb9bde5
Ethan Snow <bombitmanbomb@gmail.com>
2024-01-29 05:50:29 -0500

    wipe

--- server/scripts/setup-local-db.bat
+++
@@ -1,6 +0,0 @@
-echo %~dp0
-docker exec -i vl-db sh -c "exec mysqladmin -uroot --password=vl-password drop verseledger-app"
-docker cp create-db.sh vl-db:create-db.sh
-docker exec -i vl-db sh -c "./create-db.sh";
-docker cp verseledger-app.sql vl-db:verseledger-app.sql
-docker exec -i vl-db sh -c "exec mysql -uroot --password=vl-password verseledger-app < verseledger-app.sql"
