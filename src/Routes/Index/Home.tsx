200cc2e73e378208edb5f0fb6c2945df23347be0
ThreeCrown <st3phanadams7@gmail.com>
2024-02-01 15:08:43 -0800

    Switched Hierarchy of callback
    Moved the Hover Handler to Home.tsx as the parent passing the callback to the child fixing the style manipulation.

--- src/Routes/Index/Home.tsx
+++ src/Routes/Index/Home.tsx
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
 
 import FreelancerLoop from '../../Assets/media/MenuPage/FreelancerLoop.webm?url';
 import JobsLoop from '../../Assets/media/MenuPage/JobsLoop.webm?url';
@@ -9,24 +10,62 @@ import OrgLoop from '../../Assets/media/MenuPage/OrgLoop.webm?url';
 import { HomeNavButton } from './HomeNavButton';
 
 export const Home: React.FC<unknown> = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    console.log('Hover Start');
+    setIsHovered(true);
+    console.log(setIsHovered);
+    console.log(isHovered);
+  };
+
+  const handleMouseExit = () => {
+    console.log('Hover End');
+    setIsHovered(false);
+    console.log(setIsHovered);
+    console.log(isHovered);
+  };
   return (
-    <Box marginTop={'3em'}>
-      <HomeNavButton
-        title="Contract Ledger"
-        to="/ledger/contract"
-        videoSource={JobsLoop}
-      />
-      <HomeNavButton
-        title="Verse Market"
-        to="/ledger/versemarket"
-        videoSource={MarketLoop}
-      />
-      <HomeNavButton
-        title="Personal Ledger"
-        to="/ledger/personal"
-        videoSource={FreelancerLoop}
-      />
-      <HomeNavButton title="Org Ledger" to="/ledger/org" videoSource={OrgLoop} />
-    </Box>
+    <Transition
+      in={isHovered}
+      timeout={1000}
+      onEnter={handleMouseEnter}
+      onExit={handleMouseExit}
+    >
+      <Box marginTop={'3em'}>
+        <HomeNavButton
+          title="Contract Ledger"
+          to="/ledger/contract"
+          isHovered={isHovered}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          videoSource={JobsLoop}
+        />
+        <HomeNavButton
+          title="Verse Market"
+          to="/ledger/versemarket"
+          videoSource={MarketLoop}
+          isHovered={isHovered}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        />
+        <HomeNavButton
+          title="Personal Ledger"
+          to="/ledger/personal"
+          videoSource={FreelancerLoop}
+          isHovered={isHovered}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        />
+        <HomeNavButton
+          title="Org Ledger"
+          to="/ledger/org"
+          videoSource={OrgLoop}
+          isHovered={isHovered}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        />
+      </Box>
+    </Transition>
   );
 };
--- src/Routes/Index/HomeNavButton.tsx
+++ src/Routes/Index/HomeNavButton.tsx
@@ -3,74 +3,62 @@ import './Home.scss';
 
 import { useTheme } from '@emotion/react';
 import { alpha, Box, Typography } from '@mui/material';
-import React, { useState } from 'react';
-import { SwitchTransition, Transition } from 'react-transition-group';
+import React from 'react';
 
 type HomeNavButtonProps = {
   title: string;
   to: string;
   videoSource: string;
+  isHovered: boolean;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
 };
 
-export const HomeNavButton: React.FC<HomeNavButtonProps> = ({ title, videoSource }) => {
+export const HomeNavButton: React.FC<HomeNavButtonProps> = ({
+  title,
+  videoSource,
+  isHovered,
+  onMouseEnter,
+  onMouseLeave,
+}) => {
   // eslint-disable-next-line
   const theme = useTheme() as any;
 
-  const [isHovered, setIsHovered] = useState(false);
-  //const [isClicked, setIsClicked] = React.useState(false);
-  // useEffect(() => {
-  //const videoElement = document.getElementById('menu-video') as HTMLVideoElement;
-
-  const handleEnter = () => {
-    setIsHovered(true);
+  const width = {
+    width: isHovered ? '24em' : '18em',
   };
-
-  const handleExit = () => {
-    setIsHovered(false);
-  };
-
-  //Might try adding a useEffect hook to try and change the width before testing the video changing. Trying to test to see if the Hover check is even working.
-  /*const [boxWidth, setBoxWidth] = useState('18em');
-  useEffect(() => {
-    setBoxWidth(isHovered? '24em' : '18em');
-  }, [isHovered]);*/
 
   return (
-    <Transition in={isHovered} timeout={1000} onEnter={handleEnter} onExit={handleExit}>
-      {(state) => {
-        const width = state === 'entered' ? '24em' : '18em';
-        return (
-          <Box
-            height="7em"
-            marginTop="1em"
-            marginLeft="1em"
-            sx={{
-              position: 'relative',
-              backgroundColor: alpha(theme.palette.primary.main, 0.5),
-              zIndex: -15,
-              border: '5px ridge #065691',
-              borderRadius: '.4em',
-              width: { width },
-            }}
-            display="flex"
-          >
-            <video loop muted id="menu-video">
-              <source src={videoSource} type="video/webm" />
-            </video>
-            <Typography
-              variant="body1"
-              alignSelf="flex-end"
-              marginLeft="auto"
-              marginTop="auto"
-              padding=".5em"
-              fontSize="1.1em"
-              sx={{ color: theme.palette.text.primary }}
-            >
-              {title}
-            </Typography>
-          </Box>
-        );
+    <Box
+      width={width}
+      height="7em"
+      marginTop="1em"
+      marginLeft="1em"
+      sx={{
+        position: 'relative',
+        backgroundColor: alpha(theme.palette.primary.main, 0.5),
+        zIndex: -15,
+        border: '5px ridge #065691',
+        borderRadius: '.4em',
       }}
-    </Transition>
+      display="flex"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <video loop muted id="menu-video">
+        <source src={videoSource} type="video/webm" />
+      </video>
+      <Typography
+        variant="body1"
+        alignSelf="flex-end"
+        marginLeft="auto"
+        marginTop="auto"
+        padding=".5em"
+        fontSize="1.1em"
+        sx={{ color: theme.palette.text.primary }}
+      >
+        {title}
+      </Typography>
+    </Box>
   );
 };
