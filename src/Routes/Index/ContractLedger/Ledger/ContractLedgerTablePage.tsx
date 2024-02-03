f655f103f609224dd9290ebc641be3d35d9ed6fd
ThreeCrown <st3phanadams7@gmail.com>
2024-02-02 18:59:40 -0800

    Initial Commit
    Setup the Components needed for the Table Page

---
+++ src/Routes/Index/ContractLedger/Ledger/ContractLedgerTablePage.tsx
@@ -0,0 +1,29 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+export const ContractLedgerTablePage: React.FC<unknown> = () => {
+  return (
+    <Box>
+      <Box>
+        <h2>QuickNav Tools</h2>
+      </Box>
+      <Box>
+        <Box>
+          <h2>Gameplay Loop Contract Type</h2>
+        </Box>
+        <Box>
+          <h2>Contract Table Display Section</h2>
+          <Box>
+            <h3>Filters, Sortby & Search</h3>
+          </Box>
+          <Box>
+            <h3>Contract Cards</h3>
+          </Box>
+        </Box>
+        <Box>
+          <h2>Contract Info Display</h2>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
