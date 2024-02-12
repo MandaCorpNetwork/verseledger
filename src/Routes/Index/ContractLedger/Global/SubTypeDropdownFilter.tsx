eee04d840400c879650457cc1b8bdcf8ea682b42
ThreeCrown <st3phanadams7@gmail.com>
2024-02-12 00:16:02 -0800

    Optional chaining
    Added optional chaining to fix a break from the autocomplete box

--- src/Routes/Index/ContractLedger/Global/SubTypeDropdownFilter.tsx
+++ src/Routes/Index/ContractLedger/Global/SubTypeDropdownFilter.tsx
@@ -1,5 +1,15 @@
 import { AutoAwesomeMotion } from '@mui/icons-material';
-import { Autocomplete, Button, Checkbox, Menu, MenuItem, Typography, TextField, ListSubheader } from '@mui/material';
+import {
+  Autocomplete,
+  Button,
+  Checkbox,
+  Menu,
+  MenuItem,
+  Typography,
+  TextField,
+  ListSubheader,
+  Popover,
+} from '@mui/material';
 import React, { MouseEventHandler, useEffect, useState } from 'react';
 
 export const SubTypeDropdownFilter: React.FC<unknown> = () => {
@@ -23,36 +33,52 @@ export const SubTypeDropdownFilter: React.FC<unknown> = () => {
       values: ['escort', 'bounty', 'qrf', 'asset-protection', 'attache'],
     },
     { type: 'salvage', values: ['collection', 'procurement'] },
-    { type: 'industry', values: [ 'mining', 'refining', 'manufacturing', 'scouting'] },
+    { type: 'industry', values: ['mining', 'refining', 'manufacturing', 'scouting'] },
     { type: 'rrr', values: ['rearm', 'refuel', 'repair'] },
-    { type: 'fleet', values: ['crewman', 'outsourcing']},
+    { type: 'fleet', values: ['crewman', 'outsourcing'] },
     { type: 'proxy', values: ['middleman', 'other'] },
   ];
 
   return (
     <Button sx={{ color: 'text.primary' }}>
-      <AutoAwesomeMotion onClick={handleClick} sx={{ color: 'secondary.main', width: '55px' }} />
-      <Autocomplete
-        renderInput={(params) => (
-          <TextField { ...params} label='Sub Types' placeholder='Sub Type' />
-        )}
-        options={menuValues}
-        getOptionLabel={(option) => option.type}
-        renderOption={(option) => (
-          <div>
-            <ListSubheader>{option.type}</ListSubheader>
-            {option.values.map((value) => (
-              <MenuItem key={value} value={value}>
-                {value}
-                <Checkbox />
-              </MenuItem>
-            ))}
-          </div>
-        )}
+      <AutoAwesomeMotion
+        onClick={handleClick}
+        sx={{ color: 'secondary.main', width: '55px' }}
+      />
+      <Popover
+        id="test-menu"
+        keepMounted
         open={Boolean(anchorE1)}
-        onClose={handleClose}
         anchorEl={anchorE1}
-      ></Autocomplete>
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Autocomplete
+          renderInput={(params) => (
+            <TextField {...params} label="Sub Types" placeholder="Sub Type" />
+          )}
+          options={menuValues}
+          getOptionLabel={(option) => option.type}
+          renderOption={(option) => (
+            <div>
+              <ListSubheader>{option.type}</ListSubheader>
+              {option.values?.map((value) => (
+                <MenuItem key={value} value={value}>
+                  {value}
+                  <Checkbox />
+                </MenuItem>
+              ))}
+            </div>
+          )}
+        ></Autocomplete>
+      </Popover>
     </Button>
   );
 };
