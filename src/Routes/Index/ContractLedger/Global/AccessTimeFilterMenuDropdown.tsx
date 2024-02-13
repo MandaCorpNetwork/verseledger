b278b7f2f4ce6c23914115f99fa23d4379b690cf
ThreeCrown <st3phanadams7@gmail.com>
2024-02-13 04:49:04 -0800

    Dropdown Conversion
    Converted All Dropdowns to AutoCompletes

--- src/Routes/Index/ContractLedger/Global/AccessTimeFilterMenuDropdown.tsx
+++ src/Routes/Index/ContractLedger/Global/AccessTimeFilterMenuDropdown.tsx
@@ -1,21 +1,7 @@
-import { AccessTime } from '@mui/icons-material';
-import { Checkbox, IconButton, Menu, MenuItem, Typography } from '@mui/material';
-import React, { MouseEventHandler, useEffect, useState } from 'react';
+import { Autocomplete, TextField } from '@mui/material';
+import React from 'react';
 
 export const AccessTimeFilterMenuDropdown: React.FC<unknown> = () => {
-  const [anchorE1, setAnchorE1] = useState(null);
-  const [checked, setChecked] = useState<string[]>([]);
-  const [selectAll, setSelectAll] = useState(false);
-
-  const handleClick: MouseEventHandler<SVGSVGElement> = (e: {
-    currentTarget: React.SetStateAction<null>;
-  }) => {
-    setAnchorE1(e.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorE1(null);
-  };
-
   const menuValues = [
     'all',
     '10minutes',
@@ -27,77 +13,14 @@ export const AccessTimeFilterMenuDropdown: React.FC<unknown> = () => {
     '24plushours',
   ];
 
-  const handleCheck = (value: string) => {
-    setChecked((prev) => {
-      if (prev.includes(value)) {
-        return prev.filter((item) => item !== value);
-      } else {
-        return [...prev, value];
-      }
-    });
-  };
-
-  const handleAll = () => {
-    setChecked((prev) => {
-      if (!selectAll || checked.length !== menuValues.length) {
-        return menuValues;
-      } else {
-        return [];
-      }
-    });
-    setSelectAll((prev) => !prev);
-  };
-
-  useEffect(() => {
-    console.log('AccessTimeFilterValues: ', checked);
-  });
-
   return (
-    <IconButton sx={{ color: 'text.primary' }}>
-      <AccessTime onClick={handleClick} sx={{ color: 'secondary.main', width: '55px' }} />
-      <Menu
-        id="test-menu"
-        keepMounted
-        open={Boolean(anchorE1)}
-        anchorEl={anchorE1}
-        onClose={handleClose}
-        MenuListProps={{
-          sx: { backgroundColor: 'primary.main' },
-        }}
-      >
-        <MenuItem value={'all'} onClick={handleAll}>
-          <Checkbox checked={checked.includes('all')} />
-          <Typography>{'All'}</Typography>
-        </MenuItem>
-        <MenuItem value={'10minutes'} onClick={() => handleCheck('10minutes')}>
-          <Checkbox checked={checked.includes('10minutes')} />
-          <Typography>{'> 10 Minutes'}</Typography>
-        </MenuItem>
-        <MenuItem value={'10-30minutes'} onClick={() => handleCheck('10-30minutes')}>
-          <Checkbox checked={checked.includes('10-30minutes')} />
-          <Typography>{'10-30 Minutes'}</Typography>
-        </MenuItem>
-        <MenuItem value={'30-60minutes'} onClick={() => handleCheck('30-60minutes')}>
-          <Checkbox checked={checked.includes('30-60minutes')} />
-          <Typography>{'30-60 Minutes'}</Typography>
-        </MenuItem>
-        <MenuItem value={'1-5hours'} onClick={() => handleCheck('1-5hours')}>
-          <Checkbox checked={checked.includes('1-5hours')} />
-          <Typography>{'1-5 Hours'}</Typography>
-        </MenuItem>
-        <MenuItem value={'5-12hours'} onClick={() => handleCheck('5-12hours')}>
-          <Checkbox checked={checked.includes('5-12hours')} />
-          <Typography>{'5-12 Hours'}</Typography>
-        </MenuItem>
-        <MenuItem value={'12-24hours'} onClick={() => handleCheck('12-24hours')}>
-          <Checkbox checked={checked.includes('12-24hours')} />
-          <Typography>{'12-24 Hours'}</Typography>
-        </MenuItem>
-        <MenuItem value={'24plushours'} onClick={() => handleCheck('24plushours')}>
-          <Checkbox checked={checked.includes('24plushours')} />
-          <Typography>{'24+ Hours'}</Typography>
-        </MenuItem>
-      </Menu>
-    </IconButton>
+    <Autocomplete 
+      multiple
+      options={menuValues}
+      renderInput={(params) =>
+        <TextField {...params} label="Access Time" placeholder="Access Time" />
+      }
+      getOptionLabel={(option) => option}
+    />
   );
 };
--- src/Routes/Index/ContractLedger/Global/ContractOwnerTypeFilterMenuDropdown.tsx
+++ src/Routes/Index/ContractLedger/Global/ContractOwnerTypeFilterMenuDropdown.tsx
@@ -1,21 +1,7 @@
-import { Group } from '@mui/icons-material';
-import { Checkbox, IconButton, Menu, MenuItem, Typography } from '@mui/material';
-import React, { MouseEventHandler, useEffect, useState } from 'react';
+import { Autocomplete, TextField } from '@mui/material';
+import React from 'react';
 
 export const ContractOwnerTypeFilterMenuDropdown: React.FC<unknown> = () => {
-  const [anchorE1, setAnchorE1] = useState(null);
-  const [checked, setChecked] = useState<string[]>([]);
-  const [selectAll, setSelectAll] = useState(false);
-
-  const handleClick: MouseEventHandler<SVGSVGElement> = (e: {
-    currentTarget: React.SetStateAction<null>;
-  }) => {
-    setAnchorE1(e.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorE1(null);
-  };
-
   const menuValues = [
     'all',
     'individual',
@@ -25,74 +11,14 @@ export const ContractOwnerTypeFilterMenuDropdown: React.FC<unknown> = () => {
     '200-500members',
     '500plusmembers',
   ];
-
-  const handleCheck = (value: string) => {
-    setChecked((prev) => {
-      if (prev.includes(value)) {
-        return prev.filter((item) => item !== value);
-      } else {
-        return [...prev, value];
-      }
-    });
-  };
-
-  const handleAll = () => {
-    setChecked((prev) => {
-      if (!selectAll || checked.length !== menuValues.length) {
-        return menuValues;
-      } else {
-        return [];
-      }
-    });
-    setSelectAll((prev) => !prev);
-  };
-
-  useEffect(() => {
-    console.log('ContractOwnerTypeFilterValues: ', checked);
-  });
-
   return (
-    <IconButton sx={{ color: 'text.primary' }}>
-      <Group onClick={handleClick} sx={{ color: 'secondary.main', width: '55px' }} />
-      <Menu
-        id="test-menu"
-        keepMounted
-        open={Boolean(anchorE1)}
-        anchorEl={anchorE1}
-        onClose={handleClose}
-        MenuListProps={{
-          sx: { backgroundColor: 'primary.main' },
-        }}
-      >
-        <MenuItem value={'all'} onClick={handleAll}>
-          <Checkbox checked={checked.includes('all')} />
-          <Typography>{'All'}</Typography>
-        </MenuItem>
-        <MenuItem value={'individual'} onClick={() => handleCheck('individual')}>
-          <Checkbox checked={checked.includes('individual')} />
-          <Typography>{'Individual'}</Typography>
-        </MenuItem>
-        <MenuItem value={'1-5members'} onClick={() => handleCheck('1-5members')}>
-          <Checkbox checked={checked.includes('1-5members')} />
-          <Typography>{'Small Group'}</Typography>
-        </MenuItem>
-        <MenuItem value={'5-50members'} onClick={() => handleCheck('5-50members')}>
-          <Checkbox checked={checked.includes('5-50members')} />
-          <Typography>{'Small Business'}</Typography>
-        </MenuItem>
-        <MenuItem value={'50-200members'} onClick={() => handleCheck('50-200members')}>
-          <Checkbox checked={checked.includes('50-200members')} />
-          <Typography>{'Small Enterprise'}</Typography>
-        </MenuItem>
-        <MenuItem value={'200-500members'} onClick={() => handleCheck('200-500members')}>
-          <Checkbox checked={checked.includes('200-500members')} />
-          <Typography>{'Corporation'}</Typography>
-        </MenuItem>
-        <MenuItem value={'500plusmembers'} onClick={() => handleCheck('500plusmembers')}>
-          <Checkbox checked={checked.includes('500plusmembers')} />
-          <Typography>{'Conglomerant'}</Typography>
-        </MenuItem>
-      </Menu>
-    </IconButton>
+    <Autocomplete 
+      multiple
+      options={menuValues}
+      renderInput={(params) =>
+        <TextField {...params} label="Contract Owner Type" placeholder="Contract Owner Type" />
+      }
+      getOptionLabel={(option) => option}
+    />
   );
 };
