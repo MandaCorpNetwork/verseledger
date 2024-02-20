3bea76dbacd012ec9fcdbdfdd9cafedb722cb68d
ThreeCrown <st3phanadams7@gmail.com>
2024-02-20 00:05:03 -0800

    ContractSelection
    Updated the Modal to retrieve the selected contract and display itself over the card display.
    Started process for Loading information into the contractBriefingDisplay

---
+++ src/Components/Contracts/List/CardView/PickedContractModal.tsx
@@ -0,0 +1,48 @@
+import { Modal } from '@mui/material';
+import React from 'react';
+
+import { ContractCard } from '@/Components/Contracts/List/CardView/ContractCard';
+import { useAppSelector } from '@/Redux/hooks';
+
+type PickedContractModalProps = {
+  selectedId: number | null;
+};
+
+export const PickedContractModal: React.FC<PickedContractModalProps> = ({
+  selectedId,
+}) => {
+  const pickedContract = useAppSelector((root) =>
+    root.contracts.find((c) => c.id === selectedId),
+  );
+
+  return (
+    <>
+      {pickedContract && (
+        <Modal
+          open={selectedId !== null}
+          disableEnforceFocus={true}
+          disablePortal={true}
+          sx={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            zIndex: '1000',
+            top: '0',
+            left: '0',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingBottom: '10%',
+            '& .MuiBackdrop-root': {
+              backgroundColor: 'rgba(5, 5, 32, .68)',
+              backdropFilter: 'blur(10px)',
+              position: 'absolute',
+            },
+          }}
+        >
+          <ContractCard contract={pickedContract} />
+        </Modal>
+      )}
+    </>
+  );
+};
--- src/Components/Contracts/List/ContractLedgerContractsViewer.tsx
+++ src/Components/Contracts/List/ContractLedgerContractsViewer.tsx
@@ -1,11 +1,22 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CardorTableViewToggle } from '@/Components/Contracts/List/CardView/Card-TableViewToggle';
+import { PickedContractModal } from '@/Components/Contracts/List/CardView/PickedContractModal';
+import { selectContracts } from '@/Redux/Slices/Contracts/contractSelectors';
+import { fetchContracts } from '@/Redux/Slices/Contracts/contractThunks';
 
 import { ContractCardDisplay } from './CardView/ContractCardDisplay';
+export const ContractContext = React.createContext(null);
 
 export const ContractLedgerContractsViewer: React.FC<unknown> = () => {
+  const [selectedId, setSelectedId] = useState<number | null>(null);
+  const handleContractPick = (id: number | null) => {
+    setSelectedId(id);
+  };
+  //Contract Selection for selecting a certain Contract
+  const [selectedContract, setSelectedContract] = useState<Contract | null>(null);
+
   return (
     <Box
       id="Contract-Viewer-Box"
@@ -16,23 +27,34 @@ export const ContractLedgerContractsViewer: React.FC<unknown> = () => {
         marginTop: '1em',
         display: 'flex',
         flexDirection: 'column',
+        position: 'relative',
       }}
     >
       <Box id="Contract-Display-Toggle-Box" sx={{ padding: '.5em', marginLeft: 'auto' }}>
         <CardorTableViewToggle />
       </Box>
+      <PickedContractModal selectedId={selectedId} />
       <Box
         id="Contract-Display-Box"
         sx={{
           flexGrow: 1,
           display: 'flex',
-          overflow: 'auto',
+          position: 'relative',
+          overflow: selectedId ? 'hidden' : 'auto',
+          '&::-webkit-scrollbar': {
+            width: '10px',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'rgb(8, 29, 68)',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            borderRadius: '20px',
+            background: 'rgb(121, 192, 244, .5)',
+          },
         }}
       >
-        <ContractCardDisplay />
+        <ContractCardDisplay onPick={handleContractPick} />
       </Box>
     </Box>
   );
 };
-
-const TestContractDB = {};
