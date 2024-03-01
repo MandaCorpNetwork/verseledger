b048bc1fd86ab66134874c62b257d0f517375421
ThreeCrown <st3phanadams7@gmail.com>
2024-02-29 20:49:58 -0800

    Locations Form
    Fixed the locations form so that it rennders the final location at the bottom and up to 5 additional locations above it.

--- src/Components/Contracts/Create/StepperForms/LocationsForm.tsx
+++ src/Components/Contracts/Create/StepperForms/LocationsForm.tsx
@@ -12,19 +12,17 @@ import {
 } from '@mui/material';
 import React, { ReactElement } from 'react';
 
-type AdditionalLocationProps = {
-  count: number;
+interface LocationSelection {
   label: string;
-};
-// Props to be passed into the AdditionalLocation Component, which is what is rendered on the form when the add button is pressed.
+  type: 'final' | 'additional';
+}
 
-const additionalLocations: React.FC<AdditionalLocationProps> = ({ label }) => {
+const LocationSelectForm: React.FC<{ label: string }> = ({ label }) => {
   return (
     <FormLabel>
       {label}
       {/*{label} passes the 'Final Location' or 'Location ${locationCount}' set in the Locations Form*/}
       <FormControl>
-        {/*{locationTestDB} is a placeholder for the actual locations pulled from the API*/}
         <Autocomplete
           options={locationTestDB}
           renderInput={(params) => <TextField {...params} label="Location" />}
@@ -50,39 +48,26 @@ const additionalLocations: React.FC<AdditionalLocationProps> = ({ label }) => {
 };
 
 export const LocationsForm: React.FC<unknown> = () => {
-  interface Location {
-    label: string;
-  }
-  //This interface is for identifying the the label property of additionalLocations that is inserted into the locations state to save components to the state.
-  const [locations, setLocations] = React.useState<Location[]>([]);
-  // The locations state is used to store the components that are rendered on the form when the add button is pressed.
-  const [locationCount, setLocationCount] = React.useState(1);
-  // The locationCount state is used to increment the label property of additionalLocations that is inserted into the locations state to save components to the state.
-
-  const label = locationCount === 1 ? 'Final Location' : `Location ${locationCount}`;
-  // This is used to set the label property of additionalLocations Component so that the last component rendered on the form is 'Final Location' and the remaining ones are 'Location ${locationCount}'
+  const [locations, setLocations] = React.useState<LocationSelection[]>([]);
 
   const handleAddingLocation = () => {
-    setLocationCount(locationCount + 1);
-    //Increments the locationCount state by 1.
-    setLocations([...locations, additionalLocations({ count: locationCount, label })]);
-    //Inserts the additionalLocations Component into the locations state.
+    setLocations((prevLocations) => {
+      if (prevLocations.length >= 6) {
+        alert('You cannot add more than 6 locations to a contract.');
+        return prevLocations;
+      }
+      return [
+        ...prevLocations,
+        {
+          label:
+            prevLocations.length === 0
+              ? 'Final Location'
+              : `Location ${prevLocations.length}`,
+          type: prevLocations.length === 0 ? 'final' : 'additional',
+        },
+      ];
+    });
   };
-  // This handler is triggered by the AddLocation button.
-
-  const addLocationsComponents = locations.map((loc) => (
-    <additionalLocations key={loc.label} label={loc.label} />
-  ));
-  // This function is used to map the additionalLocations saved to the locations state to be able to be filtered to put 'Final Location' at the bottom of the form.
-
-  const finalLocation = addLocationsComponents.find(
-    (l) => l.props.label === 'Final Location',
-  );
-  // A constant used to find the additionalLocations Component with the 'Final Location' label to render below the others in the JSX.
-  const otherLocations = addLocationsComponents.filter(
-    (l) => l.props.label !== 'Final Location',
-  );
-  // A constant used to filter the additionalLocations Components to remove the one with the 'Final Location' label to render above the finalLocation const in JSX.
 
   return (
     <Box>
@@ -90,7 +75,6 @@ export const LocationsForm: React.FC<unknown> = () => {
         <FormLabel>
           Starting Location
           <FormControl>
-            {/*Below is the Starting Location Autocomplete that is required to be answered*/}
             <Autocomplete
               options={locationTestDB}
               renderInput={(params) => <TextField {...params} label="Location" />}
@@ -112,12 +96,17 @@ export const LocationsForm: React.FC<unknown> = () => {
             />
           </FormControl>
         </FormLabel>
-        {otherLocations}
-        {/*The otherLocations constant is mapped to render the additionalLocations Components above the finalLocation constant*/}
-        {finalLocation}
-        {/*The finalLocation constant is rendered below the otherLocations constant*/}
+        {locations
+          .filter((loc) => loc.type === 'additional')
+          .map((loc, index) => (
+            <LocationSelectForm key={index} label={loc.label} />
+          ))}
+        {locations
+          .filter((loc) => loc.type === 'final')
+          .map((loc, index) => (
+            <LocationSelectForm key={`final-${index}`} label={loc.label} />
+          ))}
         <IconButton onClick={handleAddingLocation}>
-          {/*The AddLocation button that triggers the handleAddingLocation handler*/}
           <AddCircleOutline color="secondary" />
         </IconButton>
       </FormGroup>
