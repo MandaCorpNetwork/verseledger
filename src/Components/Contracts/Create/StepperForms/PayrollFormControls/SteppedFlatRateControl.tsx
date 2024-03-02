be42148b22b16286bae5153dbfc3e35de6df51eb
ThreeCrown <st3phanadams7@gmail.com>
2024-03-01 19:42:32 -0800

    Error Fix
    Ran through and corrected the errors, and also changed from the state recieving components for the addClasses to recieving an object and using the object to interate components set up in the JSX

--- src/Components/Contracts/Create/StepperForms/PayrollFormControls/SteppedFlatRateControl.tsx
+++ src/Components/Contracts/Create/StepperForms/PayrollFormControls/SteppedFlatRateControl.tsx
@@ -2,17 +2,12 @@ import { AddCircleOutline } from '@mui/icons-material';
 import { Box, FormControl, FormControlLabel, IconButton, TextField } from '@mui/material';
 import React from 'react';
 
-const addPayClass = () => {
-  return (
-    <FormControl>
-      <FormControlLabel control={<TextField />} label="Pay Class" />
-      <FormControlLabel control={<TextField />} label="Pay Class Amount" />
-    </FormControl>
-  );
-};
+interface PayClass {
+  id: number;
+}
 
 export const SteppedFlatRateControl: React.FC = () => {
-  const [payClasses, setPayClasses] = React.useState([]);
+  const [payClasses, setPayClasses] = React.useState<PayClass[]>([]);
 
   const handleAddingClass = () => {
     setPayClasses((prevClasses) => {
@@ -20,17 +15,23 @@ export const SteppedFlatRateControl: React.FC = () => {
         alert('You cannot add more than 8 pay classes.');
         return prevClasses;
       }
-      return [...prevClasses, addPayClass()];
-    })
-  }
+      const newClass = { id: prevClasses.length + 1 };
+      return [...prevClasses, newClass];
+    });
+  };
 
   return (
     <Box>
       <FormControl>
         <FormControlLabel control={<TextField />} label="Pay Class" />
         <FormControlLabel control={<TextField />} label="Pay Class Amount" />
-        {payClasses.map((payClass) => payClass)}
       </FormControl>
+      {payClasses.map((payClass) => (
+        <FormControl key={payClass.id}>
+          <FormControlLabel control={<TextField />} label="Pay Class" />
+          <FormControlLabel control={<TextField />} label="Pay Class Amount" />
+        </FormControl>
+      ))}
       <IconButton onClick={handleAddingClass}>
         <AddCircleOutline color="secondary" />
       </IconButton>
