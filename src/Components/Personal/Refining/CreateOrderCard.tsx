e1fcc9ae591def6bfb84347cd2ea5ba455a382f9
ThreeCrown <st3phanadams7@gmail.com>
2024-03-19 23:34:07 -0700

    Refining App
    Broke the CreateOrderCard into boxes for the shell components

--- src/Components/Personal/Refining/CreateOrderCard.tsx
+++ src/Components/Personal/Refining/CreateOrderCard.tsx
@@ -2,18 +2,32 @@ import { Box, Button, Select, TextField, Typography } from '@mui/material';
 
 export const CreateOrderCard: React.FC<unknown> = () => {
   return (
-    <Box>
-      <Typography>Create Work Order</Typography>
-      <TextField label="Qeued" value="10 SCU" />
-      <TextField label="Total SCU" value="75 SCU" />
-      <Select label="Refinment Method" />
-      <Typography>Material</Typography>
-      <Typography>Yield</Typography>
-      <Button>Add</Button>
-      <TextField label="Total Cost" value="XXXX UEC" />
-      <TextField label="Processing Time" value="DD XX:XX" />
-      <Button>Cancel</Button>
-      <Button>Confirm</Button>
+    <Box sx={{ height: '100%' }}>
+      <Box sx={{}}>
+        <Typography>Create Work Order</Typography>
+      </Box>
+      <Box>
+        <TextField label="Qeued" value="10 SCU" sx={{ width: '8em' }} />
+        <TextField label="Total SCU" value="75 SCU" sx={{ width: '8em' }} />
+      </Box>
+      <Box>
+        <Select label="Refinment Method" />
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Typography>Material</Typography>
+        <Typography>Yield</Typography>
+      </Box>
+      <Box>
+        <Button>Add</Button>
+      </Box>
+      <Box>
+        <TextField label="Total Cost" value="XXXX UEC" />
+        <TextField label="Processing Time" value="DD XX:XX" />
+      </Box>
+      <Box>
+        <Button>Cancel</Button>
+        <Button>Confirm</Button>
+      </Box>
     </Box>
   );
 };
