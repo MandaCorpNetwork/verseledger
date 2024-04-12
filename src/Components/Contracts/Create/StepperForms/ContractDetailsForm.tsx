4258aeb4d3ffc8075f4ce67962f1a03bb11fc99e
ThreeCrown <st3phanadams7@gmail.com>
2024-04-11 19:04:59 -0700

    Update src/Components/Contracts/Create/StepperForms/ContractDetailsForm.tsx
    Co-authored-by: Ethan Snow <bombitmanbomb@gmail.com>

--- src/Components/Contracts/Create/StepperForms/ContractDetailsForm.tsx
+++ src/Components/Contracts/Create/StepperForms/ContractDetailsForm.tsx
@@ -222,7 +222,7 @@ export const SubTypeBriefingForm: React.FC<ContractDetailsForm> = ({
 
   return (
     <Box
-      data-id="subType-briefing-container"
+      data-testid="subType-briefing-container"
       sx={{
         display: 'flex',
         flexDirection: 'row',
