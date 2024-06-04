078a066a272fee940dbc8c85a033d65f6f33e03e
ThreeCrown <st3phanadams7@gmail.com>
2024-06-04 05:35:54 -0700

    Contractors Page Create Contract Settings Shells

--- services/client/src/Popups/CreateContract/pages/Contractors.tsx
+++ services/client/src/Popups/CreateContract/pages/Contractors.tsx
@@ -1,3 +1,12 @@
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Rating,
+  Switch,
+  TextField,
+} from '@mui/material';
 import React from 'react';
 
 export const Contractors: React.FC<{
@@ -5,5 +14,53 @@ export const Contractors: React.FC<{
   setFormData: React.Dispatch<React.SetStateAction<IContract>>;
 }> = (props) => {
   const { formData, setFormData } = props;
-  return <>Contractors</>;
+  return (
+    <Box data-testid="Contractors__Container" sx={{ mt: '1em' }}>
+      <Box>
+        <FormControl>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <FormLabel color="secondary">Contractor Settings</FormLabel>
+            <FormControlLabel
+              data-testid="ContractorsForm-Settings__RatingSwitch"
+              control={<Switch color="secondary" size="small" />}
+              label="Limit Rating"
+              componentsProps={{
+                typography: {
+                  variant: 'body2',
+                  color: 'text.secondary',
+                },
+              }}
+            />
+            <Rating />
+            <TextField
+              data-testid="ContractorsForm-Settings__MaxContractors"
+              label="Max Contractors"
+              color="secondary"
+              size="small"
+              variant="filled"
+              sx={{
+                width: '150px',
+                '& .MuiFilledInput-root': {
+                  backgroundColor: 'primary.dark',
+                  '&.Mui-disabled': {
+                    backgroundColor: 'rgba(0, 30, 100, 0.2)',
+                  },
+                },
+              }}
+            />
+            <FormControlLabel
+              control={<Switch color="secondary" size="small" />}
+              label="Allow Bidding After Deadline"
+              componentsProps={{
+                typography: {
+                  variant: 'body2',
+                  color: 'text.secondary',
+                },
+              }}
+            />
+          </Box>
+        </FormControl>
+      </Box>
+    </Box>
+  );
 };
