0f8bf0161fe70a813d96abb3fd66c217ceb7ac19
ThreeCrown <st3phanadams7@gmail.com>
2024-06-09 01:07:58 -0700

    Implemented PayStructure Popup

---
+++ services/client/src/Popups/Info/PayInfoDisplays.tsx
@@ -0,0 +1,267 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+export const FlatRateTab: React.FC<unknown> = () => {
+  return (
+    <Box
+      data-testid="FlatRate__Panel"
+      sx={{ display: 'flex', flexDirection: 'column', p: '.5em', height: '100%' }}
+    >
+      <Typography
+        data-testid="FlatRate__Title"
+        variant="h6"
+        align="center"
+        sx={{ color: 'secondary.main', fontWeight: 'bold', mb: '1em' }}
+      >
+        Flat Rate
+      </Typography>
+      <Typography
+        data-testid="FlatRate__PurposeTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold' }}
+      >
+        Purpose:
+      </Typography>
+      <Typography data-testid="FlatRate__PurposeContent" variant="body2">
+        This Pay Structure allows users to pay a flat fee to their contractors.
+      </Typography>
+      <Typography
+        data-testid="FlatRate__MechanicsTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold', mt: '.5em' }}
+      >
+        Mechanics:
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultHeader"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Default Pay -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        The Standard Payout upon Contract Completion*
+      </Typography>
+      <Typography
+        data-testid="FlatRate__NegotiationTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold', mt: '.5em' }}
+      >
+        Negotiation:
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Negotiation__DefaultTitle"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Default Pay -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Negotiation__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        Can be negotiated on by the contractor if Bargianing is turned on for the
+        contract.
+      </Typography>
+      <Typography
+        data-testid="FlatRate__ChangeDisclaimer"
+        variant="body2"
+        sx={{ color: 'info.main', fontSize: '.8em', mt: 'auto' }}
+      >
+        * Can be changed on a per-contractor basis in Contract Manager: Payroll
+      </Typography>
+    </Box>
+  );
+};
+
+export const PoolTab: React.FC<unknown> = () => {
+  return (
+    <Box
+      data-testid="FlatRate__Panel"
+      sx={{ display: 'flex', flexDirection: 'column', p: '.2em', height: '100%' }}
+    >
+      <Typography
+        data-testid="FlatRate__Title"
+        variant="h6"
+        align="center"
+        sx={{ color: 'secondary.main', fontWeight: 'bold', mb: '1em' }}
+      >
+        Pool Percentage
+      </Typography>
+      <Typography
+        data-testid="FlatRate__PurposeTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold' }}
+      >
+        Purpose:
+      </Typography>
+      <Typography data-testid="FlatRate__PurposeContent" variant="body2">
+        This Pay Structure allows users to pay a percentage of the total earnings
+      </Typography>
+      <Typography
+        data-testid="FlatRate__MechanicsTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold', mt: '.5em' }}
+      >
+        Mechanics:
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultHeader"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Default Pay -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        The Standard Percentage Per each contractor. Cannot be more than even split for
+        the Contractor Limit by default.*
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultHeader"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Even Split -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        This setting allows Employers to evenly split all earnings among their
+        contractors. It will automatically determine an even percentage based on the
+        number of contractors.
+      </Typography>
+      <Typography
+        data-testid="FlatRate__NegotiationTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold', mt: '.5em' }}
+      >
+        Negotiation:
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Negotiation__DefaultTitle"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Default Pay -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Negotiation__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        ercentage can be negotiated by the contractor (within the limits of even share) if
+        Bargianing is turned on for the contract.
+      </Typography>
+      <Typography
+        data-testid="FlatRate__ChangeDisclaimer"
+        variant="body2"
+        sx={{ color: 'info.main', fontSize: '.8em', mt: '.5em' }}
+      >
+        * Can be changed on a per-contractor basis in Contract Manager: Payroll
+      </Typography>
+    </Box>
+  );
+};
+
+export const TimedRateTab: React.FC<unknown> = () => {
+  return (
+    <Box
+      data-testid="FlatRate__Panel"
+      sx={{ display: 'flex', flexDirection: 'column', p: '.2em', height: '100%' }}
+    >
+      <Typography
+        data-testid="FlatRate__Title"
+        variant="h6"
+        align="center"
+        sx={{ color: 'secondary.main', fontWeight: 'bold', mb: '1em' }}
+      >
+        Timed Rate
+      </Typography>
+      <Typography
+        data-testid="FlatRate__PurposeTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold' }}
+      >
+        Purpose:
+      </Typography>
+      <Typography data-testid="FlatRate__PurposeContent" variant="body2">
+        This Pay Structure allows users to pay a specified amount at a timed interval
+      </Typography>
+      <Typography
+        data-testid="FlatRate__MechanicsTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold', mt: '.5em' }}
+      >
+        Mechanics:
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultHeader"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Default Pay -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        The Standard amount Per each Contractor*
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultHeader"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Time Interval -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Mechanics__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        This setting allows you to specify at what rate you would like to pay out the
+        default pay.
+      </Typography>
+      <Typography
+        data-testid="FlatRate__NegotiationTitle"
+        variant="body1"
+        sx={{ color: 'text.secondary', fontWeight: 'bold', mt: '.5em' }}
+      >
+        Negotiation:
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Negotiation__DefaultTitle"
+        variant="body2"
+        sx={{ color: 'secondary.light', fontWeight: 'bold', display: 'inline-flex' }}
+      >
+        Default Pay -
+      </Typography>
+      <Typography
+        data-testid="FlatRate-Negotiation__DefaultContent"
+        variant="body2"
+        sx={{ color: 'text.main' }}
+      >
+        Can be negotiated by the contractor if Bargianing is turned on.
+      </Typography>
+      <Typography
+        data-testid="FlatRate__ChangeDisclaimer"
+        variant="body2"
+        sx={{ color: 'info.main', fontSize: '.8em', mt: '.5em' }}
+      >
+        * Can be changed on a per-contractor basis in Contract Manager: Payroll
+      </Typography>
+    </Box>
+  );
+};
--- services/client/src/Popups/Info/PayStructures.tsx
+++ services/client/src/Popups/Info/PayStructures.tsx
@@ -1,12 +1,62 @@
-import { Box } from '@mui/material';
+import { Box, Tab, Tabs } from '@mui/material';
 import { VLPopup } from '@Popups/PopupWrapper/Popup';
+import React from 'react';
+
+import { FlatRateTab, PoolTab, TimedRateTab } from './PayInfoDisplays';
 
 export const POPUP_PAY_STRUCTURES = 'payStructures';
 
 export const PayStructuresPopup: React.FC = () => {
+  const [payTypeTab, setPayTypeTab] = React.useState('flat');
+
+  const handlePayTabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setPayTypeTab(newValue);
+  };
+
+  const renderTab = (tab: string) => {
+    switch (tab) {
+      case 'flat':
+        return <FlatRateTab />;
+      case 'pool':
+        return <PoolTab />;
+      case 'timed':
+        return <TimedRateTab />;
+      default:
+        return;
+    }
+  };
+
   return (
     <VLPopup name={POPUP_PAY_STRUCTURES} title="Pay Structures">
-      <Box>Whatdya know</Box>
+      <Box
+        data-testid="PayStructure-Popup__Wrapper"
+        sx={{
+          minWidth: '400px',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <Box>
+          <Tabs
+            value={payTypeTab}
+            textColor="secondary"
+            indicatorColor="secondary"
+            orientation="vertical"
+            onChange={handlePayTabChange}
+          >
+            <Tab label="Flat Rate" value="flat" />
+            <Tab label="Pool" value="pool" />
+            <Tab label="Timed Rate" value="timed" />
+            <Tab label="Participation Pool" value="partPool" disabled />
+            <Tab label="Stepped Flat Rate" value="stepFlat" disabled />
+            <Tab label="Weighted Pool" value="weightPool" disabled />
+            <Tab label="Weighted Timed Rate" value="weightTime" disabled />
+          </Tabs>
+        </Box>
+        <Box data-testid="PayStructure-TabDisplay__Wrapper" sx={{ ml: '1em' }}>
+          {renderTab(payTypeTab)}
+        </Box>
+      </Box>
     </VLPopup>
   );
 };
