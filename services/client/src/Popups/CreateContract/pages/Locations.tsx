00e039ff0d6f4a28dbf1887c6f862bfdea795e82
ThreeCrown <st3phanadams7@gmail.com>
2024-06-16 04:08:31 -0700

    WIP Locations

--- services/client/src/Popups/CreateContract/pages/Locations.tsx
+++ services/client/src/Popups/CreateContract/pages/Locations.tsx
@@ -37,142 +37,127 @@ export const Locations: React.FC<{
       sx={{
         mt: '1em',
         display: 'flex',
-        flexDirection: 'column',
+        flexDirection: 'row',
       }}
     >
-      <Box sx={{ mx: 'auto' }}>
-        <Box>
-          <LocationSearch onLocationSelect={handleLocationSelect} width="320px" />
-        </Box>
-        <FormControl sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Box
-            data-testid="LocationForm__Locations-Container"
+      <Box>
+        <LocationSearch onLocationSelect={handleLocationSelect} width="320px" />
+      </Box>
+      <FormControl sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Box
+          data-testid="LocationForm__Locations-Container"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            borderTop: '2px solid',
+            borderBottom: '2px solid',
+            borderRadius: '5px',
+            borderColor: 'secondary.main',
+            mt: '1em',
+            mb: '1em',
+            minWidth: '300px',
+            justifyContent: 'center',
+            p: '.5em',
+            alignItems: 'space-between',
+          }}
+        >
+          <TextField
+            data-testid="LocationForm__StartingLocation-Output"
+            label="Start Location"
+            color="secondary"
+            InputProps={{
+              readOnly: true,
+              startAdornment:
+                (formData.Locations?.length ?? 0) > 0 ? (
+                  <LocationChip
+                    locationId={formData.Locations?.[0] as string}
+                    onDelete={handleRemoveLocation}
+                  />
+                ) : null,
+            }}
             sx={{
               display: 'flex',
-              flexDirection: 'column',
+              width: '150px',
+              mr: '.5em',
+            }}
+          />
+          <TextField
+            data-testid="LocationForm__EndingLocation-Output"
+            label="End Location"
+            color="secondary"
+            InputProps={{
+              readOnly: true,
+              startAdornment:
+                (formData.Locations?.length ?? 0) > 1 ? (
+                  <LocationChip
+                    locationId={
+                      formData.Locations?.[
+                        (formData.Locations?.length ?? 0) - 1
+                      ] as string
+                    }
+                    onDelete={handleRemoveLocation}
+                  />
+                ) : null,
+            }}
+            sx={{
+              display: 'flex',
+              width: '150px',
+              mr: '.5em',
+            }}
+          />
+          <Box
+            data-testid="LocationForm__OtherLocation-Output"
+            sx={{
               borderTop: '2px solid',
               borderBottom: '2px solid',
               borderRadius: '5px',
-              borderColor: 'secondary.main',
-              mt: '1em',
-              mb: '1em',
+              borderColor: 'primary.main',
+              my: '.5em',
+              mx: '20%',
+              py: '.5em',
+              px: '.2em',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
+            <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+              Other Locations
+            </Typography>
             <Box
-              data-testid="LocationForm__StartandEndLocation__Wrapper"
+              data-testid="LocationForm__OtherLocation-List"
               sx={{
                 display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-around',
-                alignItems: 'center',
-                my: '1em',
+                flexDirection: 'column',
+                gap: '.5em',
+                maxHeight: '150px',
+                overflowY: 'auto',
+                p: '.5em',
+                '&::-webkit-scrollbar': {
+                  width: '10px',
+                },
+                '&::-webkit-scrollbar-track': {
+                  background: 'rgb(8, 29, 68)',
+                },
+                '&::-webkit-scrollbar-thumb': {
+                  borderRadius: '20px',
+                  background: 'rgb(121, 192, 244, .5)',
+                },
               }}
             >
-              <TextField
-                data-testid="LocationForm__StartingLocation-Output"
-                label="Start Location"
-                color="secondary"
-                InputProps={{
-                  readOnly: true,
-                  startAdornment:
-                    (formData.Locations?.length ?? 0) > 0 ? (
-                      <LocationChip
-                        locationId={formData.Locations?.[0] as string}
-                        onDelete={handleRemoveLocation}
-                      />
-                    ) : null,
-                }}
-                sx={{
-                  width: '150px',
-                  mr: '.5em',
-                }}
-              />
-              {(formData.Locations?.length ?? 0) > 1 && (
-                <TextField
-                  data-testid="LocationForm__EndingLocation-Output"
-                  label="End Location"
-                  color="secondary"
-                  InputProps={{
-                    readOnly: true,
-                    startAdornment:
-                      (formData.Locations?.length ?? 0) > 1 ? (
-                        <LocationChip
-                          locationId={
-                            formData.Locations?.[
-                              (formData.Locations?.length ?? 0) - 1
-                            ] as string
-                          }
-                          onDelete={handleRemoveLocation}
-                        />
-                      ) : null,
-                  }}
-                  sx={{
-                    width: '150px',
-                    mr: '.5em',
-                  }}
-                />
-              )}
+              {formData.Locations?.slice(1, -1).map((id) => (
+                <LocationChip locationId={id} onDelete={handleRemoveLocation} key={id} />
+              ))}
             </Box>
-            {(formData.Locations?.length ?? 0) > 2 && (
-              <Box
-                data-testid="LocationForm__OtherLocation-Output"
-                sx={{
-                  borderTop: '2px solid',
-                  borderBottom: '2px solid',
-                  borderRadius: '5px',
-                  borderColor: 'primary.main',
-                  my: '.5em',
-                  mx: '20%',
-                  py: '.5em',
-                  px: '.2em',
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              >
-                <Typography variant="body1" sx={{ color: 'text.secondary' }}>
-                  Other Locations
-                </Typography>
-                <Box
-                  data-testid="LocationForm__OtherLocation-List"
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '.5em',
-                    maxHeight: '150px',
-                    overflowY: 'auto',
-                    p: '.5em',
-                    '&::-webkit-scrollbar': {
-                      width: '10px',
-                    },
-                    '&::-webkit-scrollbar-track': {
-                      background: 'rgb(8, 29, 68)',
-                    },
-                    '&::-webkit-scrollbar-thumb': {
-                      borderRadius: '20px',
-                      background: 'rgb(121, 192, 244, .5)',
-                    },
-                  }}
-                >
-                  {formData.Locations?.slice(1, -1).map((id) => (
-                    <LocationChip
-                      locationId={id}
-                      onDelete={handleRemoveLocation}
-                      key={id}
-                    />
-                  ))}
-                </Box>
-              </Box>
-            )}
           </Box>
-        </FormControl>
-        {(formData.Locations?.length ?? 0) > 0 && (
-          <Typography variant="body2" align="right" sx={{ color: 'info.main' }}>
-            The Last Location entered will be the End Location.
-          </Typography>
-        )}
-      </Box>
+        </Box>
+      </FormControl>
+      {(formData.Locations?.length ?? 0) > 0 && (
+        <Typography variant="body2" align="right" sx={{ color: 'info.main' }}>
+          The Last Location entered will be the End Location.
+        </Typography>
+      )}
     </Box>
   );
 };
