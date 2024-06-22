927796ab39d367993e8abf9e526e84804c563831
Ethan Snow <bombitmanbomb@gmail.com>
2024-06-22 10:11:37 -0400

    add auth

--- services/client/src/Routes/api-docs/APIDocs.tsx
+++ services/client/src/Routes/api-docs/APIDocs.tsx
@@ -1,12 +1,20 @@
 import 'swagger-ui-react/swagger-ui.css';
 
 import { Box } from '@mui/material';
+import { AuthUtil } from '@Utils/AuthUtil';
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
 export const APIDocs: React.FC = () => {
   return (
     <Box sx={{ backgroundColor: '#FFF' }}>
-      <SwaggerUI url="http://localhost:3030/api-docs/swagger.json" />
+      <SwaggerUI
+        url="http://localhost:3030/api-docs/swagger.json"
+        onComplete={(swagger) => {
+          const token = AuthUtil.getAccessToken();
+          if (token == null) return;
+          swagger.preauthorizeApiKey('VLAuthAccessToken', `Bearer ${token}`);
+        }}
+      />
     </Box>
   );
 };
