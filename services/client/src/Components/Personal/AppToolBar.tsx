623953d4b6e228ec3be0ec90842e7d72a4d9f2bf
ThreeCrown <st3phanadams7@gmail.com>
2024-06-22 16:13:56 -0700

    AppToolbar Style Upgrade

--- services/client/src/Components/Personal/AppToolBar.tsx
+++ services/client/src/Components/Personal/AppToolBar.tsx
@@ -1,5 +1,5 @@
 import { Apps, Explore, TextSnippet } from '@mui/icons-material';
-import { ButtonGroup, Divider, IconButton } from '@mui/material';
+import { Box, ButtonGroup, Divider, IconButton, Tooltip } from '@mui/material';
 import { IconButtonProps } from '@mui/material/IconButton';
 import React from 'react';
 
@@ -16,14 +16,19 @@ const IconButtonWithDivider: React.FC<IconButtonWithDividerProps> = ({
   <>
     <IconButton
       {...props}
-      sx={{ color: isSelected ? 'secondary.main' : 'secondary.dark' }}
+      sx={{
+        color: isSelected ? 'secondary.main' : 'secondary.dark',
+        '&:hover': { color: 'secondary.light', transform: 'scale(1.2)' },
+        transition: 'color 0.3s, transform 0.3s',
+      }}
+      size="small"
     >
       {children}
     </IconButton>
     <Divider
       orientation="vertical"
       flexItem
-      sx={{ height: '24px', mt: 'auto', mb: 'auto' }}
+      sx={{ height: '24px', mt: 'auto', mb: 'auto', mx: '1em' }}
       color="#065691"
     />
   </>
@@ -50,36 +55,84 @@ export const AppToolBar: React.FC<AppToolBarProps> = ({
   };
 
   return (
-    <ButtonGroup
-      variant="text"
-      color="secondary"
-      size="large"
-      aria-label="text button group"
-      sx={{}}
+    <Box
+      sx={{
+        position: 'relative',
+        display: 'inline-block',
+        borderRadius: '5px',
+        overflow: 'hidden',
+        boxShadow: '0 2px 10px 4px rgba(24,252,252, 0.25)',
+        '&:hover': {
+          boxShadow: '0 2px 10px 4px rgba(14,49,243,0.45)',
+        },
+        transition: 'box-shadow 0.3s',
+      }}
     >
-      {icons.map((item, index) =>
-        index < icons.length - 1 ? (
-          <IconButtonWithDivider
-            color="secondary"
-            key={item.key}
-            isSelected={selectedApp === item.key}
-            onClick={() => handleAppChange(item.key)}
-          >
-            {item.icon}
-          </IconButtonWithDivider>
-        ) : (
-          <IconButton
-            color="secondary"
-            key={item.key}
-            sx={{
-              color: selectedApp === item.key ? 'secondary.main' : 'secondary.dark',
-            }}
-            onClick={() => handleAppChange(item.key)}
-          >
-            {item.icon}
-          </IconButton>
-        ),
-      )}
-    </ButtonGroup>
+      <ButtonGroup
+        variant="text"
+        color="secondary"
+        size="large"
+        aria-label="text button group"
+        sx={{
+          positon: 'relative',
+          borderLeft: '2px solid',
+          borderRight: '2px solid',
+          borderRadius: '5px',
+          borderColor: 'secondary.main',
+          px: '1em',
+          backgroundImage:
+            'linear-gradient(135deg, rgba(6,86,145,0.5), rgba(0,73,130,0.3))',
+
+          '&:before': {
+            content: '""',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            backgroundImage:
+              'radial-gradient(circle, rgba(255,255,255,0.12) 1px, transparent 1px)',
+            backgroundSize: '5px 5px',
+            opacity: 0.5,
+          },
+          '&:hover': {
+            backgroundImage:
+              'linear-gradient(135deg, rgba(14,49,243,0.3), rgba(8,22,80,0.5))',
+            borderColor: 'secondary.light',
+          },
+          transition: 'background-image 0.3s, border-color 0.3s',
+        }}
+      >
+        {icons.map((item, index) =>
+          index < icons.length - 1 ? (
+            <Tooltip title={item.key} key={item.key}>
+              <IconButtonWithDivider
+                color="secondary"
+                key={item.key}
+                isSelected={selectedApp === item.key}
+                onClick={() => handleAppChange(item.key)}
+              >
+                {item.icon}
+              </IconButtonWithDivider>
+            </Tooltip>
+          ) : (
+            <Tooltip title={item.key} key={item.key}>
+              <IconButton
+                color="secondary"
+                key={item.key}
+                sx={{
+                  color: selectedApp === item.key ? 'secondary.main' : 'secondary.dark',
+                  '&:hover': { color: 'secondary.light', transform: 'scale(1.2)' },
+                  transition: 'color 0.3s, transform 0.3s',
+                }}
+                onClick={() => handleAppChange(item.key)}
+              >
+                {item.icon}
+              </IconButton>
+            </Tooltip>
+          ),
+        )}
+      </ButtonGroup>
+    </Box>
   );
 };
