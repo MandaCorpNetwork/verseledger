cfd2165eb37bed0b8734ab8e329039668f7be8a7
Ethan Snow <bombitmanbomb@gmail.com>
2024-06-22 10:34:03 -0400

    add snippets

--- services/client/src/Routes/api-docs/APIDocs.tsx
+++ services/client/src/Routes/api-docs/APIDocs.tsx
@@ -4,11 +4,13 @@ import { Box } from '@mui/material';
 import { AuthUtil } from '@Utils/AuthUtil';
 import React from 'react';
 import SwaggerUI from 'swagger-ui-react';
+
 export const APIDocs: React.FC = () => {
   return (
     <Box sx={{ backgroundColor: '#FFF' }}>
       <SwaggerUI
         url="http://localhost:3030/api-docs/swagger.json"
+        requestSnippetsEnabled={true}
         onComplete={(swagger) => {
           const token = AuthUtil.getAccessToken();
           if (token == null) return;
