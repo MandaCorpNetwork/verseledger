6850e569b378c98c46c51ed54d90a681ddbc2cc1
ThreeCrown <st3phanadams7@gmail.com>
2024-07-06 02:57:11 -0700

    meh

--- services/client/src/Components/Personal/ContractManager/ContractManagerApp.tsx
+++ services/client/src/Components/Personal/ContractManager/ContractManagerApp.tsx
@@ -41,53 +41,6 @@ export const ContractManagerApp: React.FC<unknown> = () => {
       >
         <TabContext value={filters.get(QueryNames.ContractManagerTab) ?? 'employed'}>
           <Box
-            data-testid="ContractManager__TabContainer"
-            sx={{
-              mb: '1em',
-              pt: '.2em',
-              pl: '.8em',
-              pr: '.8em',
-              pb: '.2em',
-              borderLeft: '2px solid',
-              borderRight: '2px solid',
-              borderRadius: '10px',
-              borderColor: 'secondary.main',
-              alignSelf: 'center',
-              height: '10%',
-            }}
-          >
-            <TabList
-              data-testid="ContractManager__TabList"
-              orientation="horizontal"
-              onChange={handleBrowserChange}
-              indicatorColor="secondary"
-              textColor="secondary"
-              variant="fullWidth"
-            >
-              <Tab
-                data-testid="ContractManager__AcceptedTab"
-                label="Accepted"
-                value="employed"
-              />
-              <Tab
-                data-testid="ContractManager__PendingTab"
-                label="Pending"
-                value="pending"
-              />
-              <Tab
-                data-testid="ContractManager__OffersTab"
-                label="Offers"
-                value="offers"
-              />
-              <Tab data-testid="ContractManger__OwnedTab" label="Owned" value="owned" />
-              <Tab
-                data-testid="ContractManager__HistoryTab"
-                label="History"
-                value="closed"
-              />
-            </TabList>
-          </Box>
-          <Box
             data-testid="ContractManager__ContractListWrapper"
             sx={{
               display: 'flex',
@@ -96,9 +49,78 @@ export const ContractManagerApp: React.FC<unknown> = () => {
               borderBottom: '2px solid',
               borderRadius: '10px',
               borderColor: 'secondary.main',
-              height: '90%',
+              height: '100%',
+              background: 'rgba(0,30,100,0.2)',
+              backdropFilter: 'blur(20px)',
             }}
           >
+            <Box
+              data-testid="ContractManager__TabContainer"
+              sx={{
+                my: '1em',
+                px: '.8em',
+                py: '.2em',
+                borderLeft: '2px solid',
+                borderRight: '2px solid',
+                borderRadius: '10px',
+                borderColor: 'secondary.main',
+                alignSelf: 'center',
+                boxShadow: '0 0px 10px 5px rgba(24,252,252,0.25)',
+                backgroundImage:
+                  'linear-gradient(165deg, rgba(6,86,145,0.5), rgba(0,73,130,0.3))',
+                position: 'relative',
+                '&:before': {
+                  content: '""',
+                  position: 'absolute',
+                  top: 0,
+                  right: 0,
+                  bottom: 0,
+                  left: 0,
+                  backgroundImage:
+                    'radial-gradient(circle, rgba(255,255,255,0.12) 1px, transparent 1px)',
+                  backgroundSize: '5px 5px',
+                  opacity: 0.5,
+                },
+                '&:hover': {
+                  backgroundImage:
+                    'linear-gradient(135deg, rgba(14,49,243,0.3), rgba(8,22,80,0.5))',
+                  borderColor: 'secondary.light',
+                  boxShadow: '0 0 10px 5px rgba(14,49,252,.4)',
+                },
+                transition: 'all 0.3s',
+              }}
+            >
+              <TabList
+                data-testid="ContractManager__TabList"
+                orientation="horizontal"
+                onChange={handleBrowserChange}
+                indicatorColor="secondary"
+                textColor="secondary"
+                variant="fullWidth"
+              >
+                <Tab
+                  data-testid="ContractManager__AcceptedTab"
+                  label="Accepted"
+                  value="employed"
+                />
+                <Tab data-testid="ContractManger__OwnedTab" label="Owned" value="owned" />
+                <Tab
+                  data-testid="ContractManager__PendingTab"
+                  label="Pending"
+                  value="pending"
+                />
+                <Tab
+                  data-testid="ContractManager__OffersTab"
+                  label="Offers"
+                  value="offers"
+                />
+                <Tab
+                  data-testid="ContractManager__HistoryTab"
+                  label="History"
+                  value="closed"
+                />
+              </TabList>
+            </Box>
             <ContractManagerSearchTools />
             <ContractManagerContractList />
           </Box>
@@ -115,6 +137,8 @@ export const ContractManagerApp: React.FC<unknown> = () => {
           borderColor: 'secondary.main',
           borderRadius: '10px',
           padding: '.5em',
+          background: 'rgba(0,30,100,0.2)',
+          backdropFilter: 'blur(20px)',
         }}
       >
         <SelectedContractManager />
