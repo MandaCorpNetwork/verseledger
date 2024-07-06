e5390e32d1926d66311b8f418a9aaba4cb48ad1b
ThreeCrown <st3phanadams7@gmail.com>
2024-07-06 12:48:35 -0700

    Query Manager

--- services/client/src/Utils/Hooks/useURLQuery.tsx
+++ services/client/src/Utils/Hooks/useURLQuery.tsx
@@ -1,9 +1,25 @@
 import { Logger } from '@Utils/Logger';
 import { QueryNames } from '@Utils/QueryNames';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+
+interface QueryParams {
+  [key: string]: string | string[] | undefined;
+}
 
 export const useURLQuery = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [queryParams, setQueryParams] = useState<QueryParams>({});
+
+  useEffect(() => {
+    const params: QueryParams = {};
+    for (const [key, value] of searchParams.entries()) {
+      params[key] = value.toString().split(',');
+    }
+    setQueryParams(params);
+    Logger.info(`URLQueryParameters: ${JSON.stringify(params)}`);
+  }, [searchParams]);
 
   const setValue = (name: QueryNames, value: string | string[]) => {
     setSearchParams((params) => {
