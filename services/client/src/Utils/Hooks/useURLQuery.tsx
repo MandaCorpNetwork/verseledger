afcf38c0dfc9041b5960eafd26ebd43c9e9963c7
ThreeCrown <st3phanadams7@gmail.com>
2024-07-07 20:54:47 -0700

    Hooked the Contract Manager Tab to filters

--- services/client/src/Utils/Hooks/useURLQuery.tsx
+++ services/client/src/Utils/Hooks/useURLQuery.tsx
@@ -7,6 +7,42 @@ import { useSearchParams } from 'react-router-dom';
 export const useURLQuery = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const setValue = (name: QueryNames, value: string | string[]) => {
+    setSearchParams((params) => {
+      Logger.info(`Updating filters: ${params}`);
+      params.delete(name);
+      if (typeof value === 'string') {
+        params.set(name, value);
+      } else {
+        value.forEach((v) => params.append(name, v));
+      }
+      return params;
+    });
+  };
+
+  const overwriteURLQuery = (
+    newState: Partial<Record<QueryNames, string | string[]>>,
+  ) => {
+    const params = new URLSearchParams();
+    Logger.info(`Updating filters: ${JSON.stringify(params)}`);
+    Object.entries(searchParams).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) {
+        params.set(key, value.toString());
+      }
+    });
+    Object.entries(newState).forEach(([key, value]) => {
+      if (value === undefined) {
+        params.delete(key);
+      } else if (typeof value === 'string') {
+        params.set(key, value);
+      } else {
+        params.delete(key);
+        value.forEach((v) => params.append(key, v));
+      }
+    });
+    setSearchParams(params);
+  };
+
   useEffect(() => {
     const handleQueryFetchRequest = async () => {
       Logger.info(`Attempting Filtered Contract Fetch...`);
