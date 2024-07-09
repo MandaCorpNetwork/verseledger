8eb49b8474e8d6f136305ca0263d3edf256a1897
ThreeCrown <st3phanadams7@gmail.com>
2024-07-08 20:36:08 -0700

    WIP for ContractManager Filtering

--- services/server/src/controllers/v1/contracts.controller.ts
+++ services/server/src/controllers/v1/contracts.controller.ts
@@ -727,4 +727,53 @@ export class ContractController extends BaseHttpController {
     const contracts = await this.contractService.search(search!);
     return contracts;
   }
+
+  @ApiOperationGet({
+    tags: ['Contracts'],
+    description: 'Get Contracts from Bids',
+    summary: 'Get Contracts from Bids that match a user & status',
+    path: '/bids',
+    responses: {
+      200: {
+        type: 'array',
+        description: 'Found',
+        model: 'Contract',
+      },
+    },
+    consumes: [],
+    parameters: {
+      query: {
+        userId: {
+          required: true,
+          description: 'User ID to match on Bids',
+          type: 'string',
+        },
+        status: {
+          required: true,
+          description: 'Status to match on Bids',
+          type: 'string',
+        },
+      },
+    },
+    security: { VLAuthAccessToken: [] },
+  })
+  @httpGet('/bids')
+  private async getContractsByUserAndStatus(
+    @next() nextFunc: NextFunction,
+    @queryParam('userId') userId: string,
+    @queryParam('status') status: IContractBid['status'],
+  ) {
+    if (!IdUtil.isValidId(userId)) {
+      throw nextFunc(new BadParameterError('userId', '/bids/contracts'));
+    }
+    try {
+      const contracts = await this.contractService.getContractsByUserId(
+        userId,
+        status as IContractBid['status'],
+      );
+      return contracts;
+    } catch (error) {
+      throw new GenericError(400, (error as ZodError).issues);
+    }
+  }
 }
