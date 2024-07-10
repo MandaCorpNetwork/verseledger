0aa7d8ba52a30ddf46db99cd6fab6af4a395fc4b
ThreeCrown <st3phanadams7@gmail.com>
2024-07-09 22:09:46 -0700

    Updated Thunks to new compose query

--- services/client/src/Redux/Slices/Contracts/actions/fetch/fetchContracts.ts
+++ services/client/src/Redux/Slices/Contracts/actions/fetch/fetchContracts.ts
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { composeQuery } from '@Utils/composeQuery';
 import { Logger } from '@Utils/Logger';
 import { IContract } from 'vl-shared/src/schemas/ContractSchema';
 import { IContractSearch } from 'vl-shared/src/schemas/SearchSchema';
@@ -9,7 +10,9 @@ export const fetchContracts = createAsyncThunk(
   '/v1/contracts/search',
   async (params: IContractSearch) => {
     try {
-      const response = await NetworkService.GET(`/v1/contracts?${composeQuery(params)}`);
+      const response = await NetworkService.GET(
+        `/v1/contracts?${composeQuery({ search: params })}`,
+      );
       return response.data as IContract[];
     } catch (error) {
       Logger.error(error);
