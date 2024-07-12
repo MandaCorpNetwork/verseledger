96c6aded48395e76d4712c03fde71383375f46ac
ThreeCrown <st3phanadams7@gmail.com>
2024-07-12 12:07:21 -0700

    Updated Title

--- services/client/src/Components/Personal/ContractManager/ContractDisplay/SelectedContractManager.tsx
+++ services/client/src/Components/Personal/ContractManager/ContractDisplay/SelectedContractManager.tsx
@@ -1,4 +1,5 @@
 // import { LocationChip } from '@Common/Components/App/LocationChip';
+import { UserDisplay } from '@Common/Components/Users/UserDisplay';
 import { SalvageIcon, SecurityIcon } from '@Common/Definitions/CustomIcons';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import { Box, Button, Chip, Tab, TextField, Typography } from '@mui/material';
@@ -61,11 +62,6 @@ type SelectedContractManagerProps = {
 export const SelectedContractManager: React.FC<SelectedContractManagerProps> = ({
   contractId,
 }) => {
-  //const [filters] = useURLQuery();
-  // const selectedContract = useAppSelector((root) =>
-  //   pickContract(root, Number(filters.get(QueryNames.SelectedContract))),
-  // );
-
   const [contractManagerTab, setContractManagerTab] = useState<string>('contractors');
 
   const handleContractManageView = (_event: React.SyntheticEvent, newValue: string) => {
@@ -107,7 +103,9 @@ export const SelectedContractManager: React.FC<SelectedContractManagerProps> = (
             justifyContent: 'center',
             width: '30%',
           }}
-        ></Box>
+        >
+          <UserDisplay userid={contract.owner_id} />
+        </Box>
         <Box
           data-testid="SelectedContract__OverviewInfoContainer"
           sx={{
@@ -121,6 +119,26 @@ export const SelectedContractManager: React.FC<SelectedContractManagerProps> = (
             borderBottom: '2px solid',
             borderRadius: '5px',
             borderColor: 'primary.main',
+            borderLeft: '1px solid rgba(14,49,141,0.5)',
+            borderRight: '1px solid rgba(14,49,141,0.5)',
+            boxShadow: '0 5px 15px rgba(14,49,141,.8)',
+            position: 'relative',
+            '&:before': {
+              content: '""',
+              position: 'absolute',
+              top: 0,
+              bottom: 0,
+              right: 0,
+              left: 0,
+              background:
+                'linear-gradient(135deg, rgba(14,49,141,.5) 0%, rgba(8,22,80,0.5) 100%)',
+              opacity: 0.6,
+              backdropFilter: 'blur(10px)',
+              zIndex: -1,
+              backgroundImage:
+                'linear-gradient(transparent 75%, rgba(14,49,252,0.25) 5%)',
+              backgroundSize: '100% 2px',
+            },
           }}
         >
           <Box
@@ -140,7 +158,12 @@ export const SelectedContractManager: React.FC<SelectedContractManagerProps> = (
               data-testid="SelectedContract-OverviewInfo-Header__TitleWrapper"
               sx={{ display: 'flex', flexDirection: 'row', mr: 'auto' }}
             >
-              <Typography data-testid="SelectedContract__ContractTitle" variant="h4">
+              <Typography
+                data-testid="SelectedContract__ContractTitle"
+                variant="h4"
+                noWrap
+                sx={{ fontWeight: 'bold', maxWidth: '100%' }}
+              >
                 {contract.title}
               </Typography>
             </Box>
