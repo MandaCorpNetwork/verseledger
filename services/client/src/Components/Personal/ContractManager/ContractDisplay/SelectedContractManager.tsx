a93dfa788335f10f68ae8a448ae887fe49408dfc
ThreeCrown <st3phanadams7@gmail.com>
2024-07-12 13:38:40 -0700

    Updated Breifing Section

--- services/client/src/Components/Personal/ContractManager/ContractDisplay/SelectedContractManager.tsx
+++ services/client/src/Components/Personal/ContractManager/ContractDisplay/SelectedContractManager.tsx
@@ -671,14 +671,36 @@ export const SelectedContractManager: React.FC<SelectedContractManagerProps> = (
               flexDirection: 'column',
               p: '.5em',
               mb: '1em',
+              position: 'relative',
+              borderLeft: '1px solid rgba(14,49,141,0.5)',
+              borderRight: '1px solid rgba(14,49,141,0.5)',
+              boxShadow: '0 5px 15px rgba(14,49,141,.8)',
+              '&:before': {
+                content: '""',
+                position: 'absolute',
+                top: 0,
+                bottom: 0,
+                right: 0,
+                left: 0,
+                background:
+                  'linear-gradient(135deg, rgba(14,49,141,.5) 0%, rgba(8,22,80,0.5) 100%)',
+                opacity: 0.6,
+                backdropFilter: 'blur(10px)',
+                zIndex: -1,
+                backgroundImage:
+                  'linear-gradient(transparent 75%, rgba(14,49,252,0.25) 5%)',
+                backgroundSize: '100% 2px',
+              },
             }}
           >
             <Typography
               data-testid="SelectedContract-Briefing__BriefingTitle"
+              variant="body2"
               sx={{
                 backgroundColor: 'rgba(14,49,141,.25)',
                 borderRadius: '10px',
                 pl: '1em',
+                fontWeight: 'bold',
               }}
             >
               Briefing
@@ -713,6 +735,7 @@ export const SelectedContractManager: React.FC<SelectedContractManagerProps> = (
                   textWrap: 'wrap',
                   whiteSpace: 'normal',
                   wordBreak: 'break-all',
+                  pl: '.5em',
                 }}
               >
                 aslkgdnjrdalkrjngkqanjglpnjagrlnjaldkrjnglainjrglkanjlvkrjnakldfnjvalkdnjflkajndvlkanjdvlknavlkdnjvdlksajnvdlksanjvdlknjalkdvfnjajlkdvnjlakjdnvlkjsfghsdfghdkskjnlsk;nfgksnfdk;lnkgjksjnfgnjskl;fnjgk;dlsjngfl;kjnsdlkgfnjsdkjnfglksjn
