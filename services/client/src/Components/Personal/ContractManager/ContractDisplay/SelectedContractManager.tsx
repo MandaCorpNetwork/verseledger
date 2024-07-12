85ebb76307b6d1e68eb5544dec9dabedc9d65120
ThreeCrown <st3phanadams7@gmail.com>
2024-07-12 14:10:10 -0700

    Fixed error on Archetype

--- services/client/src/Components/Personal/ContractManager/ContractDisplay/SelectedContractManager.tsx
+++ services/client/src/Components/Personal/ContractManager/ContractDisplay/SelectedContractManager.tsx
@@ -87,7 +87,7 @@ export const SelectedContractManager: React.FC<SelectedContractManagerProps> = (
 
   const dispatch = useAppDispatch();
 
-  const archetypeDisplay = React.useMemo(() => {
+  React.useEffect(() => {
     const selectedArchetype = archetypes.find((option) =>
       option.subTypes.some((subtype) => subtype.value === contract.subtype),
     );
@@ -875,6 +875,37 @@ export const SelectedContractManager: React.FC<SelectedContractManagerProps> = (
                   indicatorColor="secondary"
                   textColor="secondary"
                   variant="fullWidth"
+                  sx={{
+                    borderLeft: '2px solid',
+                    borderRight: '2px solid',
+                    borderRadius: '5px',
+                    borderColor: 'secondary.main',
+                    px: '1em',
+                    py: '.2em',
+                    boxShadow: '0 0px 5px 2px rgba(24,252,252,0.25)',
+                    backgroundImage:
+                      'linear-gradient(165deg, rgba(6,86,145,0.5), rgba(0,73,130,0.3))',
+                    position: 'relative',
+                    '&:before': {
+                      content: '""',
+                      position: 'absolute',
+                      top: 0,
+                      right: 0,
+                      bottom: 0,
+                      left: 0,
+                      backgroundImage:
+                        'radial-gradient(circle, rgba(255,255,255,0.12) 1px, transparent 1px)',
+                      backgroundSize: '5px 5px',
+                      opacity: 0.5,
+                    },
+                    '&:hover': {
+                      backgroundImage:
+                        'linear-gradient(135deg, rgba(14,49,243,0.3), rgba(8,22,80,0.5))',
+                      borderColor: 'secondary.light',
+                      boxShadow: '0 0 5px 2px rgba(14,49,252,.4)',
+                    },
+                    transition: 'all 0.3s',
+                  }}
                 >
                   <Tab label="Contractors" value="contractors" />
                   <Tab disabled label="Payroll" value="payroll" />
