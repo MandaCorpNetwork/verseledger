d7f693b9835e3df6334a03260f685b71608919f6
ThreeCrown <st3phanadams7@gmail.com>
2024-07-12 17:01:19 -0700

    Switched UserChip to accepting UserObject

--- services/client/src/Popups/UserInvite/UserInvite.tsx
+++ services/client/src/Popups/UserInvite/UserInvite.tsx
@@ -1,22 +1,23 @@
-// import { UserSearch } from '@Common/Components/App/UserSearch';
-// import { UserChip } from '@Common/Components/Users/UserChip';
+import { UserSearch } from '@Common/Components/App/UserSearch';
+import { UserChip } from '@Common/Components/Users/UserChip';
 import { Box } from '@mui/material';
 import { VLPopup } from '@Popups/PopupWrapper/Popup';
 import { useAppDispatch } from '@Redux/hooks';
 import { closePopup } from '@Redux/Slices/Popups/popups.actions';
+import { Logger } from '@Utils/Logger';
 import React from 'react';
+import { IUser } from 'vl-shared/src/schemas/UserSchema';
 
 export const POPUP_USER_INVITE = 'userInvite';
 
 export const UserInvitePopup: React.FC = () => {
-  //@ts-expect-error unused currently
   const [selectedUsers, setSelectedUsers] = React.useState([]); //eslint-disable-line @typescript-eslint/no-unused-vars
   const dispatch = useAppDispatch();
 
-  // const handleAddUser = (selectedUser: string) => {
-  //   setSelectedUsers((selectedUsers) => ({ ...selectedUsers, selectedUser }));
-  //   console.log(selectedUsers);
-  // };
+  const handleAddUser = (selectedUser: IUser | null) => {
+    setSelectedUsers((selectedUsers) => ({ ...selectedUsers, selectedUser }));
+    Logger.info(selectedUsers);
+  };
 
   return (
     <VLPopup
@@ -24,12 +25,12 @@ export const UserInvitePopup: React.FC = () => {
       title="Invite Users"
       onSubmit={() => dispatch(closePopup(POPUP_USER_INVITE))}
     >
-      <Box>{/* <UserSearch width="250px" onUserSelect={handleAddUser} /> */}</Box>
       <Box>
-        {/* {selectedUsers !== null &&
-          selectedUsers.map((user) => (
-            <UserChip key={user} userid={user.userid} size="small" />
-          ))} */}
+        <UserSearch width="250px" onUserSelect={handleAddUser} />
+      </Box>
+      <Box>
+        {selectedUsers !== null &&
+          selectedUsers.map((user) => <UserChip key={user} user={user} size="small" />)}
       </Box>
     </VLPopup>
   );
