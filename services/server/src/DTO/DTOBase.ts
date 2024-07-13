94ce18cf9addb973ecceccdaffe0d63f97082293
Ethan Snow <bombitmanbomb@gmail.com>
2024-07-12 22:24:45 -0400

    Fix null mapping bug in DtoMapper

--- services/server/src/DTO/DTOBase.ts
+++ services/server/src/DTO/DTOBase.ts
@@ -15,9 +15,14 @@ export abstract class DTOBase<T> {
         contstructorMap[key] != null
           ? Array.isArray(props[key])
             ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              props[key].map((v: any) => new contstructorMap[key](v))
-            : new contstructorMap[key](props[key])
+              props[key].map((v: any) =>
+                v != null ? new contstructorMap[key](v) : null,
+              )
+            : props[key] != null
+              ? new contstructorMap[key](props[key])
+              : null
           : props[key];
+      if (value == null) continue;
       if (typeof value === 'function') continue;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (this as any)[key] = value;
