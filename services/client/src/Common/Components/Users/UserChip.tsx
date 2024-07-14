bb8fbd160d761791d4423d1d3f9e8f30a587dae8
ThreeCrown <st3phanadams7@gmail.com>
2024-07-14 00:57:41 -0700

    Finished Pagination on ContractLedger

--- services/client/src/Common/Components/Users/UserChip.tsx
+++ services/client/src/Common/Components/Users/UserChip.tsx
@@ -1,21 +1,29 @@
 import { Avatar, Chip } from '@mui/material';
 import { POPUP_PLAYER_CARD } from '@Popups/PlayerCard/PlayerCard';
-import { useAppDispatch } from '@Redux/hooks';
+import { useAppDispatch, useAppSelector } from '@Redux/hooks';
 import { openPopup } from '@Redux/Slices/Popups/popups.actions';
+import { selectUserById } from '@Redux/Slices/Users/userSelectors';
 import { Logger } from '@Utils/Logger';
 import { IUser } from 'vl-shared/src/schemas/UserSchema';
 
 type UserChipProps = {
-  user: IUser;
+  user?: IUser;
+  user_id?: string;
   size: 'small' | 'medium';
   onDelete?: () => void;
 };
 
-export const UserChip: React.FC<UserChipProps> = ({ user, size, onDelete }) => {
+export const UserChip: React.FC<UserChipProps> = ({ user, size, onDelete, user_id }) => {
   const dispatch = useAppDispatch();
 
+  const pullPlayer = useAppSelector((state) =>
+    user_id ? selectUserById(state, user_id) : undefined,
+  );
+
+  const player = user || pullPlayer;
+
   const handlePlayerCardOpen = () => {
-    dispatch(openPopup(POPUP_PLAYER_CARD, { userid: user.id }));
+    dispatch(openPopup(POPUP_PLAYER_CARD, { userid: player?.id }));
   };
 
   const handleDeleteUser = () => {
@@ -24,7 +32,7 @@ export const UserChip: React.FC<UserChipProps> = ({ user, size, onDelete }) => {
     }
   };
 
-  Logger.info(`UserChip Recieved ID: ${user.id}`);
+  Logger.info(`UserChip Recieved ID: ${player?.id}`);
 
   return (
     <>
@@ -33,8 +41,8 @@ export const UserChip: React.FC<UserChipProps> = ({ user, size, onDelete }) => {
         variant="outlined"
         size={size}
         onClick={handlePlayerCardOpen}
-        label={user.displayName}
-        avatar={<Avatar src={user.pfp} />}
+        label={player?.displayName}
+        avatar={<Avatar src={player?.pfp} />}
         onDelete={onDelete ? handleDeleteUser : undefined}
         sx={{
           maxWidth: '150px',
