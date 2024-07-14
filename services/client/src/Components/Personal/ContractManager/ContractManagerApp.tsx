db575e19d7fe66936a5c26c04d1a755b0e1ec58a
ThreeCrown <st3phanadams7@gmail.com>
2024-07-14 01:23:07 -0700

    Logic Error, need bidPagination

--- services/client/src/Components/Personal/ContractManager/ContractManagerApp.tsx
+++ services/client/src/Components/Personal/ContractManager/ContractManagerApp.tsx
@@ -135,7 +135,7 @@ export const ContractManagerApp: React.FC<unknown> = () => {
           };
           handleFetchBids(bidParams).then((contractIds) => {
             const contractParams: IContractSearch = {
-              page: 0,
+              page: page - 1,
               limit: 25,
               status: ['BIDDING', 'INPROGRESS'],
               contractId: contractIds,
@@ -164,7 +164,7 @@ export const ContractManagerApp: React.FC<unknown> = () => {
           };
           handleFetchBids(bidParams).then((contractIds) => {
             const contractParams: IContractSearch = {
-              page: 0,
+              page: page - 1,
               limit: 25,
               status: ['BIDDING', 'INPROGRESS'],
               contractId: contractIds,
@@ -182,7 +182,7 @@ export const ContractManagerApp: React.FC<unknown> = () => {
           };
           handleFetchBids(bidParams).then((contractIds) => {
             const contractParams: IContractSearch = {
-              page: 0,
+              page: page - 1,
               limit: 25,
               status: ['BIDDING', 'INPROGRESS'],
               contractId: contractIds,
