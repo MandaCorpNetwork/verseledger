498d07de7c691f06e3ef62a52b604c5d80ef296b
ThreeCrown <st3phanadams7@gmail.com>
2024-07-20 15:28:43 -0700

    Added FirstLayerGlassBox Styled Component

---
+++ services/client/src/Common/Components/Boxes/FirstLayerGlassBox.tsx
@@ -0,0 +1,15 @@
+import { Box, styled } from '@mui/material';
+const FirstLayerGlassBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  borderTop: '2px solid',
+  borderBottom: '2px solid',
+  borderRadius: '10px',
+  borderColor: theme.palette.secondary.main,
+  height: '100%',
+  background: 'rgba(0,30,100,0.2)',
+  backdropFilter: 'blur(20px)',
+  padding: '.5em',
+}));
+
+export default FirstLayerGlassBox;
