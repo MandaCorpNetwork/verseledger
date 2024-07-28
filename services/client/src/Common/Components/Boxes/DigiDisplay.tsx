d84c64fd9864caec8fca24c73a1ec8ed49b549dc
ThreeCrown <st3phanadams7@gmail.com>
2024-07-27 22:15:13 -0700

    Finished Bid UI Elements Update

--- services/client/src/Common/Components/Boxes/DigiDisplay.tsx
+++ services/client/src/Common/Components/Boxes/DigiDisplay.tsx
@@ -10,12 +10,17 @@ const DigiDisplay = styled(Box)(({ theme }) => ({
   borderTop: '1px solid',
   borderBottom: '1px solid',
   borderColor: theme.palette.primary.main,
-  boxShadow: '0 3px 10px rgba(0,0,0,.3)',
+  boxShadow:
+    '0 3px 10px rgba(0,0,0,.3), 0 6px 12px rgba(0,0,0,.3), inset 0 1px 2px rgba(14,35,141,.3), inset 0 -1px 2px rgba(0,0,0,0.1)',
   overflow: 'hidden',
   transition: 'all 0.1s ease-in-out',
+  position: 'relative',
   color: theme.palette.text.secondary,
+  backgroundImage:
+    'linear-gradient(135deg, rgba(14,49,141,.4) 0%, rgba(8,22,80,0.4) 100%)',
   '&:hover': {
-    boxShadow: '0 6px 20px rgba(0,0,0,.4)',
+    boxShadow:
+      '0 6px 12px rgba(0,0,0,.4), 0 12px 24px rgba(0,0,0,.5), inset 0 1px 2px rgba(14,35,141,.3), inset 0 -1px 2px rgba(0,0,0,0.1)',
     backdropFilter: 'blur(10px)',
     color: theme.palette.secondary.main,
   },
