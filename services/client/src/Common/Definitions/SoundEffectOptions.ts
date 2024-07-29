f5be33a59944699905d4e799f46194d2ad8b4240
ThreeCrown <st3phanadams7@gmail.com>
2024-07-29 03:09:40 -0700

    Massive Soundpack update to add other Manufacturers

--- services/client/src/Common/Definitions/SoundEffectOptions.ts
+++ services/client/src/Common/Definitions/SoundEffectOptions.ts
@@ -24,6 +24,60 @@ import AnvilSuccess from '../../Assets/Sounds/Anvil/anvilSuccess.wav?url';
 import AnvilToggleOff from '../../Assets/Sounds/Anvil/anvilToggleOff.wav?url';
 import AnvilToggleOn from '../../Assets/Sounds/Anvil/anvilToggleOn.wav?url';
 import AnvilWarning from '../../Assets/Sounds/Anvil/anvilWarning.wav?url';
+import CnouClick from '../../Assets/Sounds/CNOU/cnouClick.wav?url';
+import CnouClose from '../../Assets/Sounds/CNOU/cnouClose.wav?url';
+import CnouDenied from '../../Assets/Sounds/CNOU/cnouDenied.wav?url';
+import CnouError from '../../Assets/Sounds/CNOU/cnouError.wav?url';
+import CnouGeneralNotify from '../../Assets/Sounds/CNOU/cnouGeneralNotify.wav?url';
+import CnouLoading from '../../Assets/Sounds/CNOU/cnouLoading.wav?url';
+import CnouMessageNotify from '../../Assets/Sounds/CNOU/cnouMessageNotify.wav?url';
+import CnouNavigate from '../../Assets/Sounds/CNOU/cnouNavigate.wav?url';
+import CnouOpen from '../../Assets/Sounds/CNOU/cnouOpen.wav?url';
+import CnouSend from '../../Assets/Sounds/CNOU/cnouSend.wav?url';
+import CnouSuccess from '../../Assets/Sounds/CNOU/cnouSuccess.wav?url';
+import CnouToggleOff from '../../Assets/Sounds/CNOU/cnouToggleOff.wav?url';
+import CnouToggleOn from '../../Assets/Sounds/CNOU/cnouToggleOn.wav?url';
+import CnouWarning from '../../Assets/Sounds/CNOU/cnouWarning.wav?url';
+import DrakeClose from '../../Assets/Sounds/Drake/drakeClose.wav?url';
+import DrakeDenied from '../../Assets/Sounds/Drake/drakeDenied.wav?url';
+import DrakeError from '../../Assets/Sounds/Drake/drakeError.wav?url';
+import DrakeGeneralNotify from '../../Assets/Sounds/Drake/drakeGeneralNotify.wav?url';
+import DrakeLoading from '../../Assets/Sounds/Drake/drakeLoading.wav?url';
+import DrakeMessageNotify from '../../Assets/Sounds/Drake/drakeMessageNotify.wav?url';
+import DrakeNavigate from '../../Assets/Sounds/Drake/drakeNavigate.wav?url';
+import DrakeOpen from '../../Assets/Sounds/Drake/drakeOpen.wav?url';
+import DrakeSend from '../../Assets/Sounds/Drake/drakeSend.wav?url';
+import DrakeSuccess from '../../Assets/Sounds/Drake/drakeSuccess.wav?url';
+import DrakeToggleOff from '../../Assets/Sounds/Drake/drakeToggleOff.wav?url';
+import DrakeToggleOn from '../../Assets/Sounds/Drake/drakeToggleOn.wav?url';
+import DrakeWarning from '../../Assets/Sounds/Drake/drakeWarning.wav?url';
+import OriginClick from '../../Assets/Sounds/Origin/originClick.wav?url';
+import OriginClose from '../../Assets/Sounds/Origin/originClose.wav?url';
+import OriginDenied from '../../Assets/Sounds/Origin/originDenied.wav?url';
+import OriginError from '../../Assets/Sounds/Origin/originError.wav?url';
+import OriginGeneralNotify from '../../Assets/Sounds/Origin/originGeneralNotify.wav?url';
+import OriginLoading from '../../Assets/Sounds/Origin/originLoading.wav?url';
+import OriginMessageNotify from '../../Assets/Sounds/Origin/originMessageNotify.wav?url';
+import OriginNavigate from '../../Assets/Sounds/Origin/originNavigate.wav?url';
+import OriginOpen from '../../Assets/Sounds/Origin/originOpen.wav?url';
+import OriginSend from '../../Assets/Sounds/Origin/originSend.wav?url';
+import OriginSuccess from '../../Assets/Sounds/Origin/originSuccess.wav?url';
+import OriginToggleOff from '../../Assets/Sounds/Origin/originToggleOff.wav?url';
+import OriginToggleOn from '../../Assets/Sounds/Origin/originToggleOn.wav?url';
+import OriginWarning from '../../Assets/Sounds/Origin/originWarning.wav?url';
+import RSIClose from '../../Assets/Sounds/RSI/rsiClose.wav?url';
+import RSIDenied from '../../Assets/Sounds/RSI/rsiDenied.wav?url';
+import RSIError from '../../Assets/Sounds/RSI/rsiError.wav?url';
+import RSIGeneralNotify from '../../Assets/Sounds/RSI/rsiGeneralNotify.wav?url';
+import RSILoading from '../../Assets/Sounds/RSI/rsiLoading.wav?url';
+import RSIMessageNotify from '../../Assets/Sounds/RSI/rsiMessageNotify.wav?url';
+import RSINavigate from '../../Assets/Sounds/RSI/rsiNavigate.wav?url';
+import RSIOpen from '../../Assets/Sounds/RSI/rsiOpen.wav?url';
+import RSISend from '../../Assets/Sounds/RSI/rsiSend.wav?url';
+import RSISuccess from '../../Assets/Sounds/RSI/rsiSuccess.wav?url';
+import RSIToggleOff from '../../Assets/Sounds/RSI/rsiToggleOff.wav?url';
+import RSIToggleOn from '../../Assets/Sounds/RSI/rsiToggleOn.wav?url';
+import RSIWarning from '../../Assets/Sounds/RSI/rsiWarning.wav?url';
 import SysClose from '../../Assets/Sounds/System/sysClose.wav?url';
 import SysHover from '../../Assets/Sounds/System/sysHover.wav?url';
 import SysNavigate from '../../Assets/Sounds/System/sysNavigate.wav?url';
@@ -44,6 +98,22 @@ export const soundEffectOptions = {
       src: new Audio(AnvilClose),
       name: 'Anvil - Close',
     },
+    {
+      src: new Audio(CnouClose),
+      name: 'CNOU - Close',
+    },
+    {
+      src: new Audio(DrakeClose),
+      name: 'Drake - Close',
+    },
+    {
+      src: new Audio(OriginClose),
+      name: 'Origin - Close',
+    },
+    {
+      src: new Audio(RSIClose),
+      name: 'RSI - Close',
+    },
   ],
   denied: [
     {
@@ -53,6 +123,22 @@ export const soundEffectOptions = {
     {
       src: new Audio(AnvilDenied),
       name: 'Anvil - Denied',
+    },
+    {
+      src: new Audio(CnouDenied),
+      name: 'CNOU - Denied',
+    },
+    {
+      src: new Audio(DrakeDenied),
+      name: 'Drake - Denied',
+    },
+    {
+      src: new Audio(OriginDenied),
+      name: 'Origin - Denied',
+    },
+    {
+      src: new Audio(RSIDenied),
+      name: 'RSI - Denied',
     },
   ],
   error: [
@@ -64,6 +150,22 @@ export const soundEffectOptions = {
       src: new Audio(AnvilError),
       name: 'Anvil - Error',
     },
+    {
+      src: new Audio(CnouError),
+      name: 'CNOU - Error',
+    },
+    {
+      src: new Audio(DrakeError),
+      name: 'Drake - Error',
+    },
+    {
+      src: new Audio(OriginError),
+      name: 'Origin - Error',
+    },
+    {
+      src: new Audio(RSIError),
+      name: 'RSI - Error',
+    },
   ],
   generalNotify: [
     {
@@ -73,6 +175,22 @@ export const soundEffectOptions = {
     {
       src: new Audio(AnvilGeneralNotify),
       name: 'Anvil - General Notify',
+    },
+    {
+      src: new Audio(CnouGeneralNotify),
+      name: 'CNOU - General Notify',
+    },
+    {
+      src: new Audio(DrakeGeneralNotify),
+      name: 'Drake - General Notify',
+    },
+    {
+      src: new Audio(OriginGeneralNotify),
+      name: 'Origin - General Notify',
+    },
+    {
+      src: new Audio(RSIGeneralNotify),
+      name: 'RSI - General Notify',
     },
   ],
   loading: [
@@ -84,6 +202,22 @@ export const soundEffectOptions = {
       src: new Audio(AnvilLoading),
       name: 'Anvil - Loading',
     },
+    {
+      src: new Audio(CnouLoading),
+      name: 'CNOU - Loading',
+    },
+    {
+      src: new Audio(DrakeLoading),
+      name: 'Drake - Loading',
+    },
+    {
+      src: new Audio(OriginLoading),
+      name: 'Origin - Loading',
+    },
+    {
+      src: new Audio(RSILoading),
+      name: 'RSI - Loading',
+    },
   ],
   messageNotify: [
     {
@@ -93,6 +227,22 @@ export const soundEffectOptions = {
     {
       src: new Audio(AnvilMessageNotify),
       name: 'Anvil - Message Notify',
+    },
+    {
+      src: new Audio(CnouMessageNotify),
+      name: 'CNOU - Message Notify',
+    },
+    {
+      src: new Audio(DrakeMessageNotify),
+      name: 'Drake - Message Notify',
+    },
+    {
+      src: new Audio(OriginMessageNotify),
+      name: 'Origin - Message Notify',
+    },
+    {
+      src: new Audio(RSIMessageNotify),
+      name: 'RSI - Message Notify',
     },
   ],
   navigate: [
@@ -108,6 +258,22 @@ export const soundEffectOptions = {
       src: new Audio(AnvilNavigate),
       name: 'Anvil - Navigate',
     },
+    {
+      src: new Audio(CnouNavigate),
+      name: 'CNOU - Navigate',
+    },
+    {
+      src: new Audio(DrakeNavigate),
+      name: 'Drake - Navigate',
+    },
+    {
+      src: new Audio(OriginNavigate),
+      name: 'Origin - Navigate',
+    },
+    {
+      src: new Audio(RSINavigate),
+      name: 'RSI - Navigate',
+    },
   ],
   open: [
     {
@@ -122,6 +288,22 @@ export const soundEffectOptions = {
       src: new Audio(AnvilOpen),
       name: 'Anvil - Open',
     },
+    {
+      src: new Audio(CnouOpen),
+      name: 'CNOU - Open',
+    },
+    {
+      src: new Audio(DrakeOpen),
+      name: 'Drake - Open',
+    },
+    {
+      src: new Audio(OriginOpen),
+      name: 'Origin - Open',
+    },
+    {
+      src: new Audio(RSIOpen),
+      name: 'RSI - Open',
+    },
   ],
   send: [
     {
@@ -131,6 +313,22 @@ export const soundEffectOptions = {
     {
       src: new Audio(AnvilSend),
       name: 'Anvil - Send',
+    },
+    {
+      src: new Audio(CnouSend),
+      name: 'CNOU - Send',
+    },
+    {
+      src: new Audio(DrakeSend),
+      name: 'Drake - Send',
+    },
+    {
+      src: new Audio(OriginSend),
+      name: 'Origin - Send',
+    },
+    {
+      src: new Audio(RSISend),
+      name: 'RSI - Send',
     },
   ],
   success: [
@@ -142,6 +340,22 @@ export const soundEffectOptions = {
       src: new Audio(AnvilSuccess),
       name: 'Anvil - Success',
     },
+    {
+      src: new Audio(CnouSuccess),
+      name: 'CNOU - Success',
+    },
+    {
+      src: new Audio(DrakeSuccess),
+      name: 'Drake - Success',
+    },
+    {
+      src: new Audio(OriginSuccess),
+      name: 'Origin - Success',
+    },
+    {
+      src: new Audio(RSISuccess),
+      name: 'RSI - Success',
+    },
   ],
   toggleOff: [
     {
@@ -151,6 +365,22 @@ export const soundEffectOptions = {
     {
       src: new Audio(AnvilToggleOff),
       name: 'Anvil - Toggle Off',
+    },
+    {
+      src: new Audio(CnouToggleOff),
+      name: 'CNOU - Toggle Off',
+    },
+    {
+      src: new Audio(DrakeToggleOff),
+      name: 'Drake - Toggle Off',
+    },
+    {
+      src: new Audio(OriginToggleOff),
+      name: 'Origin - Toggle Off',
+    },
+    {
+      src: new Audio(RSIToggleOff),
+      name: 'RSI - Toggle Off',
     },
   ],
   toggleOn: [
@@ -162,6 +392,22 @@ export const soundEffectOptions = {
       src: new Audio(AnvilToggleOn),
       name: 'Anvil - Toggle On',
     },
+    {
+      src: new Audio(CnouToggleOn),
+      name: 'CNOU - Toggle On',
+    },
+    {
+      src: new Audio(DrakeToggleOn),
+      name: 'Drake - Toggle On',
+    },
+    {
+      src: new Audio(OriginToggleOn),
+      name: 'Origin - Toggle On',
+    },
+    {
+      src: new Audio(RSIToggleOn),
+      name: 'RSI - Toggle On',
+    },
   ],
   warning: [
     {
@@ -171,6 +417,22 @@ export const soundEffectOptions = {
     {
       src: new Audio(AnvilWarning),
       name: 'Anvil - Warning',
+    },
+    {
+      src: new Audio(CnouWarning),
+      name: 'CNOU - Warning',
+    },
+    {
+      src: new Audio(DrakeWarning),
+      name: 'Drake - Warning',
+    },
+    {
+      src: new Audio(OriginWarning),
+      name: 'Origin - Warning',
+    },
+    {
+      src: new Audio(RSIWarning),
+      name: 'RSI - Warning',
     },
   ],
   hover: [
@@ -183,6 +445,14 @@ export const soundEffectOptions = {
     {
       src: new Audio(SysPrimaryClick),
       name: 'System - Primary Click',
+    },
+    {
+      src: new Audio(CnouClick),
+      name: 'CNOU - Click',
+    },
+    {
+      src: new Audio(OriginClick),
+      name: 'Origin - Click',
     },
   ],
 };
@@ -246,6 +516,86 @@ export const soundEffectPacks = {
       hover: soundEffectOptions.hover[0].src,
       open: soundEffectOptions.open[2].src,
       clickMain: soundEffectOptions.clickMain[0].src,
+    },
+  },
+  cnouPack: {
+    name: 'CNOU Pack',
+    pack: {
+      close: soundEffectOptions.close[3].src,
+      denied: soundEffectOptions.denied[2].src,
+      error: soundEffectOptions.error[2].src,
+      generalNotify: soundEffectOptions.generalNotify[2].src,
+      loading: soundEffectOptions.loading[2].src,
+      messageNotify: soundEffectOptions.messageNotify[2].src,
+      navigate: soundEffectOptions.navigate[3].src,
+      send: soundEffectOptions.send[2].src,
+      success: soundEffectOptions.success[2].src,
+      toggleOff: soundEffectOptions.toggleOff[2].src,
+      toggleOn: soundEffectOptions.toggleOn[2].src,
+      warning: soundEffectOptions.warning[2].src,
+      hover: soundEffectOptions.hover[0].src,
+      open: soundEffectOptions.open[3].src,
+      clickMain: soundEffectOptions.clickMain[1].src,
+    },
+  },
+  drakePack: {
+    name: 'Drake Pack',
+    pack: {
+      close: soundEffectOptions.close[4].src,
+      denied: soundEffectOptions.denied[3].src,
+      error: soundEffectOptions.error[3].src,
+      generalNotify: soundEffectOptions.generalNotify[3].src,
+      loading: soundEffectOptions.loading[3].src,
+      messageNotify: soundEffectOptions.messageNotify[3].src,
+      navigate: soundEffectOptions.navigate[4].src,
+      send: soundEffectOptions.send[3].src,
+      success: soundEffectOptions.success[3].src,
+      toggleOff: soundEffectOptions.toggleOff[3].src,
+      toggleOn: soundEffectOptions.toggleOn[3].src,
+      warning: soundEffectOptions.warning[3].src,
+      hover: soundEffectOptions.hover[0].src,
+      open: soundEffectOptions.open[4].src,
+      clickMain: soundEffectOptions.clickMain[1].src,
+    },
+  },
+  originPack: {
+    name: 'Origin Pack',
+    pack: {
+      close: soundEffectOptions.close[5].src,
+      denied: soundEffectOptions.denied[4].src,
+      error: soundEffectOptions.error[4].src,
+      generalNotify: soundEffectOptions.generalNotify[4].src,
+      loading: soundEffectOptions.loading[4].src,
+      messageNotify: soundEffectOptions.messageNotify[4].src,
+      navigate: soundEffectOptions.navigate[5].src,
+      send: soundEffectOptions.send[4].src,
+      success: soundEffectOptions.success[4].src,
+      toggleOff: soundEffectOptions.toggleOff[4].src,
+      toggleOn: soundEffectOptions.toggleOn[4].src,
+      warning: soundEffectOptions.warning[4].src,
+      hover: soundEffectOptions.hover[0].src,
+      open: soundEffectOptions.open[5].src,
+      clickMain: soundEffectOptions.clickMain[1].src,
+    },
+  },
+  rsiPack: {
+    name: 'RSI Pack',
+    pack: {
+      close: soundEffectOptions.close[6].src,
+      denied: soundEffectOptions.denied[5].src,
+      error: soundEffectOptions.error[5].src,
+      generalNotify: soundEffectOptions.generalNotify[5].src,
+      loading: soundEffectOptions.loading[5].src,
+      messageNotify: soundEffectOptions.messageNotify[5].src,
+      navigate: soundEffectOptions.navigate[6].src,
+      send: soundEffectOptions.send[5].src,
+      success: soundEffectOptions.success[5].src,
+      toggleOff: soundEffectOptions.toggleOff[5].src,
+      toggleOn: soundEffectOptions.toggleOn[5].src,
+      warning: soundEffectOptions.warning[5].src,
+      hover: soundEffectOptions.hover[0].src,
+      open: soundEffectOptions.open[6].src,
+      clickMain: soundEffectOptions.clickMain[1].src,
     },
   },
 };
