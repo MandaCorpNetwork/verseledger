e7fde536aba8c99fc55ebad74919e7726e6fe8bb
Ethan Snow <bombitmanbomb@gmail.com>
2024-07-31 18:32:05 -0400

    Fix linting issues

--- services/server/.eslintrc.cjs
+++ services/server/.eslintrc.cjs
@@ -1,3 +1,5 @@
+const { rules } = require('../client/.eslintrc');
+
 module.exports = {
   root: true,
   env: { es2020: true },
@@ -10,6 +12,16 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_[^_].*$|^_$',
+        varsIgnorePattern: '^_[^_].*$|^_$',
+        caughtErrorsIgnorePattern: '^_[^_].*$|^_$',
+      },
+    ],
   },
   ignorePatterns: ['build', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
