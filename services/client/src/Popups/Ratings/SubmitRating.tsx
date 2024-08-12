7c38bb4ec574061dae9e3e8b241ed47d0a9d5462
ThreeCrown <st3phanadams7@gmail.com>
2024-08-12 08:00:32 -0700

    Fixed not passing ContractId to the thunk

--- services/client/src/Popups/Ratings/SubmitRating.tsx
+++ services/client/src/Popups/Ratings/SubmitRating.tsx
@@ -1,3 +1,4 @@
+import { useSoundEffect } from '@/AudioManager';
 import GlassBox from '@Common/Components/Boxes/GlassBox';
 import { UserRatingField } from '@Common/Components/Custom/DigiField/UserRatingField';
 import { Divider, FormControl, Typography } from '@mui/material';
@@ -9,6 +10,7 @@ import { postDelayRating } from '@Redux/Slices/Notifications/actions/postDelayRa
 import { closePopup, openPopup } from '@Redux/Slices/Popups/popups.actions';
 import { postNewContractRating } from '@Redux/Slices/Users/Actions/postContractRating';
 import { selectUserById } from '@Redux/Slices/Users/userSelectors';
+import { enqueueSnackbar } from 'notistack';
 import React from 'react';
 import { IContract } from 'vl-shared/src/schemas/ContractSchema';
 import {
@@ -31,6 +33,7 @@ export const SubmitRatingPopup: React.FC<SubmitRatingPopupProps> = ({
   const [formData, setFormData] = React.useState<ICreateUserRatingBody[]>([]);
   const currentUser = useAppSelector(selectCurrentUser);
   const dispatch = useAppDispatch();
+  const { playSound } = useSoundEffect();
 
   //const recentRatings = TODO: get the recent ratings to disabler the user from rating the same user twice
 
@@ -38,11 +41,13 @@ export const SubmitRatingPopup: React.FC<SubmitRatingPopupProps> = ({
     const initialData = users.map((user) => ({
       reciever_id: user.id,
       rating_value: 0,
+      ...(contract && { contract_id: contract.id }),
     }));
     if (owner && !initialData.find((data) => data.reciever_id === owner.id)) {
       initialData.push({
         reciever_id: owner.id,
         rating_value: 0,
+        ...(contract && { contract_id: contract.id }),
       });
     }
     setFormData(initialData);
@@ -79,7 +84,19 @@ export const SubmitRatingPopup: React.FC<SubmitRatingPopupProps> = ({
         contract_id: contract.id,
         ratings: formData as ICreateUserRatingBody[],
       };
-      dispatch(postNewContractRating(ratingData as ICreateContractRatingsBody));
+      dispatch(postNewContractRating(ratingData as ICreateContractRatingsBody)).then(
+        (res) => {
+          if (postNewContractRating.fulfilled.match(res)) {
+            enqueueSnackbar('Ratings Submitted', { variant: 'success' });
+            playSound('send');
+          } else {
+            enqueueSnackbar(`Error Submitting Ratings ${res.error}`, {
+              variant: 'error',
+            });
+            playSound('error');
+          }
+        },
+      );
     }
   };
 
