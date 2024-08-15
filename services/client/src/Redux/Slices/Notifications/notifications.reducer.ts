523637ec8422a8f6c16b6494f49c48b9824e1f0d
ThreeCrown <st3phanadams7@gmail.com>
2024-08-15 10:20:53 -0700

    Updated the Notifications reducer

--- services/client/src/Redux/Slices/Notifications/notifications.reducer.ts
+++ services/client/src/Redux/Slices/Notifications/notifications.reducer.ts
@@ -2,6 +2,8 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { fetchNotifications } from './actions/getNotifications';
 import { fetchUnreadCount } from './actions/getUnreadCount';
+import { markAllRead } from './actions/markAllRead';
+import { markRead } from './actions/patchMarkRead';
 
 const notificationsReducer = createSlice({
   name: 'notifications',
@@ -16,20 +18,32 @@ const notificationsReducer = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(fetchUnreadCount.fulfilled, (_state, action) => {
-      _state.unreadNotifications = (action.payload as { unread: number }).unread;
-    });
-    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
-      const notifications = action.payload as {
-        id: string;
-        text: string;
-        createdAt: Date;
-      }[];
-      state.notificationsMap = {};
-      for (const notif of notifications) {
-        state.notificationsMap[notif.id] = notif;
-      }
-    });
+    builder
+      .addCase(fetchUnreadCount.fulfilled, (_state, action) => {
+        _state.unreadNotifications = (action.payload as { unread: number }).unread;
+      })
+      .addCase(fetchNotifications.fulfilled, (state, action) => {
+        const notifications = action.payload as {
+          id: string;
+          text: string;
+          createdAt: Date;
+        }[];
+        state.notificationsMap = {};
+        for (const notif of notifications) {
+          state.notificationsMap[notif.id] = notif;
+        }
+      })
+      .addCase(markRead.fulfilled, (state, action) => {
+        const notificationId = action.meta.arg;
+        if (state.notificationsMap[notificationId]) {
+          delete state.notificationsMap[notificationId];
+          state.unreadNotifications -= 1;
+        }
+      })
+      .addCase(markAllRead.fulfilled, (state) => {
+        state.notificationsMap = {};
+        state.unreadNotifications = 0;
+      });
   },
 });
 
