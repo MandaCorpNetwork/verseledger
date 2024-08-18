021d155b1f514f7ba2eb7cdc3d14181ed81cf0e5
CodeBreaker1995 <eugenepetrie@live.com>
2024-08-18 06:15:07 -0400

    Small Update to UserPage.tsx

--- services/client/src/Routes/User/UserPage.tsx
+++ services/client/src/Routes/User/UserPage.tsx
@@ -1,9 +1,21 @@
 import { VLViewport } from '@Common/Components/Boxes/VLViewport';
-import { Box } from '@mui/material';
+import { Avatar, Box, Stack } from '@mui/material';
+import { useAppSelector } from '@Redux/hooks';
+import { selectUserById } from '@Redux/Slices/Users/userSelectors';
 
-export const UserPage: React.FC<unknown> = () => {
+export type UserPageProps = {
+  /** The user ID. */
+  userid: string;
+};
+
+export const UserPage: React.FC<UserPageProps> = ({ userid }) => {
+  const user = useAppSelector((state) => selectUserById(state, userid));
+
   return (
     <VLViewport data-testid="UserPage_PageContainer">
+      <Stack direction="row" spacing={2}>
+        <Avatar alt="Test Avatar" src={user?.pfp} />
+      </Stack>
       <Box>UserPage</Box>
     </VLViewport>
   );
