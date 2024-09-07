d0e7879e43709286d097bb65f3078b826b5dec0c
Ethan Snow <bombitmanbomb@gmail.com>
2024-09-07 19:14:59 -0400

    Notification Type

--- packages/vl-shared/src/schemas/NotificationSchema.ts
+++ packages/vl-shared/src/schemas/NotificationSchema.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
 
-export const NotificationSchema = z.object({});
+export const NotificationSchema = z.object({
+  id: z.string().max(26),
+  user_id: z.string().max(26),
+  read: z.boolean(),
+  message: z.string(),
+  action: z.string().max(255).nullish(),
+});
 
 export type INotification = z.infer<typeof NotificationSchema>;
