7f351aa187b1d2d3c0679081be22b439509ff02a
Ethan Snow <bombitmanbomb@gmail.com>
2024-09-08 16:55:50 -0400

    Efficient array map

--- services/server/src/v1/models/notifications/mapping/NotificationsToNotificationDTOArrayMapper.ts
+++ services/server/src/v1/models/notifications/mapping/NotificationsToNotificationDTOArrayMapper.ts
@@ -1,18 +1,17 @@
 import { Mapper } from '@/infrastructure/Mapper';
 import { Notification } from '../notification.model';
 import { NotificationDTO } from './NotificationDTO';
-import { INotification } from 'vl-shared/src/schemas/NotificationSchema';
 
 export class NotificationsToNotificationDTOArrayMapper extends Mapper<
   Notification[],
   NotificationDTO
 > {
-  public static override map(artifacts: Notification[]): NotificationDTO[] {
-    const response: NotificationDTO[] = [];
-    for (const artifact of artifacts) {
-      const notification: INotification = artifact.get();
-      response.push(new NotificationDTO(notification));
-    }
-    return response;
+  public static override map(
+    artifacts: Notification[] = [],
+  ): NotificationDTO[] {
+    return artifacts.map((artifact) => {
+      const notif = artifact.get();
+      return new NotificationDTO(notif);
+    });
   }
 }
