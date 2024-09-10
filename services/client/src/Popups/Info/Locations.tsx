f37ae3e90642b669b016dc0f2729dc242646d566
ThreeCrown <st3phanadams7@gmail.com>
2024-09-10 02:18:37 -0700

    Completed Location Popup Layout

--- services/client/src/Popups/Info/Locations.tsx
+++ services/client/src/Popups/Info/Locations.tsx
@@ -1,9 +1,19 @@
+import { DigiField } from '@Common/Components/Custom/DigiField/DigiField';
 import { ReadOnlyField } from '@Common/Components/TextFields/ReadOnlyField';
-import { Box, Button } from '@mui/material';
+import { ContentCopy } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { Gauge, SparkLineChart } from '@mui/x-charts';
 import { VLPopup } from '@Popups/PopupWrapper/Popup';
 import { useAppSelector } from '@Redux/hooks';
 import { selectLocationById } from '@Redux/Slices/Locations/locationSelectors';
-import { Logger } from '@Utils/Logger';
 
 export type LocationInfoProps = {
   locationId: string;
@@ -12,7 +22,6 @@ export type LocationInfoProps = {
 export const POPUP_LOCATION_INFO = 'locationInfo';
 
 export const LocationInfoPopup: React.FC<LocationInfoProps> = ({ locationId }) => {
-  Logger.info('LocationPopup', locationId);
   const location = useAppSelector((state) => selectLocationById(state, locationId));
 
   return (
@@ -20,24 +29,156 @@ export const LocationInfoPopup: React.FC<LocationInfoProps> = ({ locationId }) =
       data-testid="Location__Popup"
       name={POPUP_LOCATION_INFO}
       title={location ? location.short_name : 'Unknown Location'}
+      sx={{
+        minWidth: '500px',
+      }}
     >
       <Box
         data-testid="LocationPopup__ContentWrapper"
         sx={{ display: 'flex', flexDirection: 'column' }}
       >
-        <Box data-testid="LocationPopup__ButtonWrapper">
-          <Button>Set Location</Button>
-          <Button>Report Crime</Button>
-          <Button>Open Explorer</Button>
+        <Box
+          data-testid="LocationPopup__ButtonWrapper"
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            mb: '1em',
+          }}
+        >
+          <Button variant="outlined" color="success" size="small">
+            Set Location
+          </Button>
+          <Button variant="outlined" color="error" size="small">
+            Report Crime
+          </Button>
+          <Button variant="outlined" color="secondary" size="small">
+            Open Explorer
+          </Button>
         </Box>
-        <Box data-testid="LocationPopup__TimeDataWrapper">
+        <Box
+          data-testid="LocationPopup__TimeDataWrapper"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
           <ReadOnlyField label="Local Time" />
-          <ReadOnlyField label="StarRise Time" />
-          <ReadOnlyField label="StarSet Time" />
+          <Box
+            data-testid="LocationPopup-TimeData__LightTime_Wrapper"
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'space-between',
+              mb: '.5em',
+            }}
+          >
+            <ReadOnlyField label="StarRise Time" />
+            <ReadOnlyField label="StarSet Time" />
+          </Box>
         </Box>
-        <Box data-testid="LocationPopup__">
-          <Box></Box>
-          <Box></Box>
+        <Box data-testid="LocationPopup__InformationWrapper">
+          <Box
+            data-testid="LocationPopup-Information__StaticInfo_Wrapper"
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              gap: '1em',
+            }}
+          >
+            <Box data-testid="LocationPopup-Information-StaticInfo__Coordiantes_Wrapper">
+              <Typography data-testid="LocationPopup-Information-StaticInfo__Coordiantes_Title">
+                Coordinates
+              </Typography>
+              <Tooltip
+                data-testid="LocationPopup-Information-StaticInfo__Coordiantes_Tooltip"
+                title={`x: ${location?.x}, y: ${location?.y}, z: ${location?.z}`}
+              >
+                <TextField
+                  data-testid="LocationPopup-Information-StaticInfo__Coordiante_Field"
+                  size="small"
+                  value={`x: ${location?.x}, y: ${location?.y}, z: ${location?.z}`}
+                  fullWidth
+                  slotProps={{
+                    input: {
+                      inputProps: {
+                        sx: {
+                          textAlign: 'center',
+                        },
+                      },
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton>
+                            <ContentCopy />
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    },
+                  }}
+                />
+              </Tooltip>
+            </Box>
+            <Box
+              data-testid="LocationPopup-Information-StaticInfo__Parents_Wrapper"
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+              }}
+            >
+              <DigiField label="Moon" sx={{ minWidth: '100px' }}>
+                Moon
+              </DigiField>
+              <DigiField label="Planet" sx={{ minWidth: '100px' }}>
+                Planet
+              </DigiField>
+              <DigiField label="Star" sx={{ minWidth: '100px' }}>
+                Star
+              </DigiField>
+            </Box>
+          </Box>
+          <Box
+            data-testid="LocationPopup-Information__UserBasedInfo_Wrapper"
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: '.5em',
+              alignItems: 'center',
+              width: '100%',
+              justifyContent: 'stretch',
+              mt: '1em',
+            }}
+          >
+            <Box
+              data-testid="LocationPopup-Information-UserBasedInfo__PopulationChart_Container"
+              sx={{ display: 'flex', flexDirection: 'column', flexGrow: 3 }}
+            >
+              <Typography data-testid="LocationPopup-Information-UserBasedInfo__PopulationChart_Title">
+                Population
+              </Typography>
+              <SparkLineChart
+                height={100}
+                data={[2500, 800, 1500, 900, 700, 600, 3200]}
+              />
+            </Box>
+            <Box
+              data-testid="LocationPopup-Information-UserBasedInfo__SafteyMeter_Wrapper"
+              sx={{ display: 'flex', flexDirection: 'column' }}
+            >
+              <Typography>Saftey Rating</Typography>
+              <Gauge
+                value={75}
+                startAngle={-110}
+                endAngle={110}
+                text={`Danger`}
+                width={120}
+                height={100}
+              />
+            </Box>
+          </Box>
         </Box>
       </Box>
     </VLPopup>
