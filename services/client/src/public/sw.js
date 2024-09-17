676f81b8b0732d7028c13de33b86c7d63959ad5a
Ethan Snow <bombitmanbomb@gmail.com>
2024-09-16 21:43:14 -0400

    Make a web-app (#557)
    * Make a web-app
    
    * Update pwa images
    
    * ignore dev dist
    
    * fix prettier
    
    * ignore dev dist

---
+++ services/client/src/public/sw.js
@@ -0,0 +1,21 @@
+import { clientsClaim } from 'workbox-core';
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching';
+
+self.skipWaiting();
+clientsClaim();
+cleanupOutdatedCaches();
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('push', (event) => {
+  const payload = event.data ? event.data.text() : 'no payload';
+
+  event.waitUntil(
+    new Promise((resolve) => {
+      if (Notification.permission === 'granted') {
+        if (!document.hidden) return resolve(true);
+        resolve(self.registration.showNotification('Verseledger', { body: payload }));
+      } else resolve(false);
+    }),
+  );
+});
