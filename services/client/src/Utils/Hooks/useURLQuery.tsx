b1c721c43c1dde6733f02b4814254371ae6e513d
ThreeCrown <st3phanadams7@gmail.com>
2024-09-18 19:05:23 -0700

    Fixes Rerender for the useURLQuery, removes Conditional in Tooltip Appbar

--- services/client/src/Utils/Hooks/useURLQuery.tsx
+++ services/client/src/Utils/Hooks/useURLQuery.tsx
@@ -1,45 +1,50 @@
 import { Logger } from '@Utils/Logger';
 import { QueryNames } from '@Utils/QueryNames';
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export const useURLQuery = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const setValue = (name: QueryNames, value: string | string[]) => {
-    setSearchParams((params) => {
-      Logger.info(`Updating filters: ${params}`);
-      params.delete(name);
-      if (typeof value === 'string') {
-        params.set(name, value);
-      } else {
-        value.forEach((v) => params.append(name, v));
-      }
-      return params;
-    });
-  };
+  const setFilters = React.useCallback(
+    (name: QueryNames, value: string | string[]) => {
+      setSearchParams((params) => {
+        Logger.info(`Updating filters: ${params}`);
+        params.delete(name);
+        if (typeof value === 'string') {
+          params.set(name, value);
+        } else {
+          value.forEach((v) => params.append(name, v));
+        }
+        return params;
+      });
+    },
+    [setSearchParams],
+  );
 
-  const overwriteURLQuery = (
-    newState: Partial<Record<QueryNames, string | string[]>>,
-  ) => {
-    const params = new URLSearchParams();
-    Logger.info(`Updating filters: ${JSON.stringify(params)}`);
-    Object.entries(searchParams).forEach(([key, value]) => {
-      if (value !== undefined && value !== null) {
-        params.set(key, value.toString());
-      }
-    });
-    Object.entries(newState).forEach(([key, value]) => {
-      if (value === undefined) {
-        params.delete(key);
-      } else if (typeof value === 'string') {
-        params.set(key, value);
-      } else {
-        params.delete(key);
-        value.forEach((v) => params.append(key, v));
-      }
-    });
-    setSearchParams(params);
-  };
+  const overwriteURLQuery = React.useCallback(
+    (newState: Partial<Record<QueryNames, string | string[]>>) => {
+      const params = new URLSearchParams();
+      Logger.info(`Updating filters: ${JSON.stringify(params)}`);
+      Object.entries(searchParams).forEach(([key, value]) => {
+        if (value !== undefined && value !== null) {
+          params.set(key, value.toString());
+        }
+      });
+      Object.entries(newState).forEach(([key, value]) => {
+        if (value === undefined) {
+          params.delete(key);
+        } else if (typeof value === 'string') {
+          params.set(key, value);
+        } else {
+          params.delete(key);
+          value.forEach((v) => params.append(key, v));
+        }
+      });
+      setSearchParams(params);
+    },
+    [searchParams, setSearchParams],
+  );
 
-  return [searchParams, setValue, overwriteURLQuery] as const;
+  return [searchParams, setFilters, overwriteURLQuery];
 };
