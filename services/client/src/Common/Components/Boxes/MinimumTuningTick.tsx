5b910c54243472829f180091f701f2ebb123801e
ThreeCrown <st3phanadams7@gmail.com>
2024-10-10 16:12:36 -0700

    Added Minimum Tick Box

---
+++ services/client/src/Common/Components/Boxes/MinimumTuningTick.tsx
@@ -0,0 +1,28 @@
+import { Divider } from '@mui/material';
+import React from 'react';
+
+import { TuningTick } from './TuningTick';
+
+type MinimumTuningTickProps = {
+  minimumTicks: number;
+};
+export const MinimumTuningTick: React.FC<MinimumTuningTickProps> = ({ minimumTicks }) => {
+  return (
+    <TuningTick
+      data-testid="ShipTuning-TuningEditor-ConfigGroup__Config__TickBox"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: `${20 * minimumTicks}px`,
+        justifyContent: 'space-around',
+      }}
+    >
+      {[...Array(minimumTicks - 1)].map((_, index) => (
+        <Divider
+          key={index}
+          sx={{ width: '70%', mx: 'auto', borderBottomWidth: '2px' }}
+        />
+      ))}
+    </TuningTick>
+  );
+};
