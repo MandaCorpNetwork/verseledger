617fc027fd6eef466d8d279b591353b1086007af
ThreeCrown <st3phanadams7@gmail.com>
2024-10-17 09:56:04 -0700

    Contract Ledger Refactor Pt 1
    SideBar

---
+++ services/client/src/Components/Contracts/Ledger/SidePanel/CollapsedButtons.tsx
@@ -0,0 +1,91 @@
+import { contractArchetypes } from '@Common/Definitions/Contracts/ContractArchetypes';
+import { AddCircle, Search } from '@mui/icons-material';
+import { IconButton, Slide, Tooltip } from '@mui/material';
+import React from 'react';
+
+type CollapsedButtonsProps = {
+  isExpanded: boolean;
+  isMobile: boolean;
+  isTablet: boolean;
+  searchClick: () => void;
+  setFilter: (value: ContractArchetype) => void;
+  currentFilters: ContractArchetype[];
+  openCreate: () => void;
+};
+export const CollapsedButtons: React.FC<CollapsedButtonsProps> = ({
+  isExpanded,
+  isMobile,
+  isTablet,
+  searchClick,
+  setFilter,
+  currentFilters,
+  openCreate,
+}) => {
+  return (
+    <Slide
+      data-testid="ContractLedger-SidePanel__CollapsedButtons_Slide"
+      direction="right"
+      in={!isExpanded}
+      timeout={{ enter: 800, exit: 300 }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {(isMobile || isTablet) && (
+          <IconButton
+            data-testid="ContractLedger-SidePanel-CollapsedButtons__MobileSearchToggle_Button"
+            size="small"
+            onClick={searchClick}
+          >
+            <Search fontSize="large" />
+          </IconButton>
+        )}
+        <Tooltip
+          title="Create Contract"
+          placement="right"
+          disableInteractive
+          enterDelay={500}
+          enterNextDelay={2000}
+          leaveDelay={300}
+        >
+          <IconButton
+            data-testid="ContractLedger-SidePanel-CollapsedButtons__CreateContract_Button"
+            onClick={openCreate}
+            size="small"
+          >
+            <AddCircle fontSize="large" />
+          </IconButton>
+        </Tooltip>
+        {contractArchetypes.map((archetype) => (
+          <Tooltip
+            key={archetype.archetype}
+            title={archetype.archetype}
+            placement="right"
+            disableInteractive
+            enterDelay={300}
+          >
+            <IconButton
+              data-testid={`ContractLedger-ColumnOne-ArchetypeButton__${archetype.archetype}_Button`}
+              onClick={() => setFilter(archetype.archetype)}
+              sx={{
+                color: currentFilters.includes(archetype.archetype)
+                  ? 'secondary.main'
+                  : 'primary.light',
+              }}
+            >
+              {React.cloneElement(archetype.archetypeIcon, {
+                fontSize: 'large',
+                color: 'inherit',
+              })}
+            </IconButton>
+          </Tooltip>
+        ))}
+      </div>
+    </Slide>
+  );
+};
--- services/client/src/Components/Contracts/Ledger/SidePanel/ContractLedgerLoopButton.tsx
+++ services/client/src/Components/Contracts/Ledger/SidePanel/ContractLedgerLoopButton.tsx
@@ -1,24 +1,21 @@
 ///<reference types="vite/client" />
-import './ContractLedgerLoopButton.css';
+import '../ContractLedgerApp.css';
 
-import { Button, SxProps, Typography } from '@mui/material';
-import type { verseOSTheme } from '@Themes/VerseOS';
-import { useURLQuery } from '@Utils/Hooks/useURLQuery';
-import { QueryNames } from '@Utils/QueryNames';
-import React, { useMemo, useState } from 'react';
+import { Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
 
 type ContractLedgerLoopButtonProps = {
   title: string;
   videoSource: string;
-  value: string;
-  sx?: SxProps<typeof verseOSTheme>;
+  onClick: () => void;
+  selected: boolean;
 };
 
 export const ContractLedgerLoopButton: React.FC<ContractLedgerLoopButtonProps> = ({
   title,
   videoSource,
-  value,
-  sx,
+  onClick,
+  selected,
 }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_isHovered, setIsHovered] = useState(false);
@@ -36,36 +33,12 @@ export const ContractLedgerLoopButton: React.FC<ContractLedgerLoopButtonProps> =
     video.currentTime = 0;
   };
   //Hovering animation
-
-  const { searchParams, setFilters } = useURLQuery();
-
-  const currentFilterValues = useMemo(() => {
-    const archetypeFilters = searchParams.getAll(QueryNames.Archetype);
-    return Array.isArray(archetypeFilters) ? archetypeFilters : [archetypeFilters];
-  }, [searchParams]);
-
-  const handleArchetypeChange = (value: string) => {
-    const video = document.getElementById(videoSource) as HTMLVideoElement;
-    if (currentFilterValues.includes(value)) {
-      video.play();
-    } else {
-      video.pause();
-      video.currentTime = 0;
-    }
-    setFilters(
-      QueryNames.Archetype,
-      currentFilterValues.includes(value)
-        ? currentFilterValues.filter((archetype) => archetype !== value)
-        : [...currentFilterValues, value],
-    );
-  };
-
   return (
     <Button
       id="contract-loop-button"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      onClick={() => handleArchetypeChange(value)}
+      onClick={() => onClick}
       data-testid="contract-loop-button"
       sx={{
         color: 'text.primary',
@@ -74,9 +47,7 @@ export const ContractLedgerLoopButton: React.FC<ContractLedgerLoopButtonProps> =
         height: '5em',
         display: 'flex',
         border: '3px ridge',
-        borderColor: currentFilterValues.includes(value)
-          ? 'secondary.main'
-          : 'primary.dark',
+        borderColor: selected ? 'secondary.main' : 'primary.dark',
         borderRadius: '.4em',
         zIndex: '3',
         marginTop: '1em',
@@ -89,7 +60,6 @@ export const ContractLedgerLoopButton: React.FC<ContractLedgerLoopButtonProps> =
           color: 'text.secondary',
           borderColor: 'primary.main',
         },
-        ...sx,
       }}
     >
       <video loop muted id={videoSource} className="loop-video">
