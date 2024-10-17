dcc4fc52fff05c20235a70da3b1b60f0a7945162
ThreeCrown <st3phanadams7@gmail.com>
2024-10-17 09:56:04 -0700

    Contract Ledger Refactor Pt 1
    SideBar

--- services/client/src/Components/Contracts/Ledger/ContractLedgerApp.tsx
+++ services/client/src/Components/Contracts/Ledger/ContractLedgerApp.tsx
@@ -1,13 +1,12 @@
 import { ContractDisplayContainer } from '@Components/Contracts/Ledger/Details/ContractDisplayContainer';
 import { LedgerBrowser } from '@Components/Contracts/Ledger/LedgerBrowser';
-import { LedgerSidePanel } from '@Components/Contracts/Ledger/LedgerSidePanel';
+import { LedgerSidePanel } from '@Components/Contracts/Ledger/SidePanel/LedgerSidePanel';
 import { Box } from '@mui/material';
 import React from 'react';
 
 /**
- * ### ContractLedgerPage
+ * ### ContractLedgerApp
  * @summary
- * The main page for the Contract Ledger.
  * It displays a list of contracts to display and search tools to find contracts.
  * It also contains navigation buttons to open the Contract Manager or create a new contract.
  * - In Desktop, it displays a selected contract in a side panel.
@@ -19,11 +18,11 @@ export const ContractLedgerApp: React.FC<unknown> = () => {
     <Box
       data-testid="ContractLedger__LedgerWrapper"
       sx={{
-        py: '1em',
         display: 'flex',
         flexDirection: 'row',
         height: '100%',
         width: '100%',
+        gap: '1em',
       }}
     >
       <LedgerSidePanel openMobileSearch={setMobileSearchOpen} />
