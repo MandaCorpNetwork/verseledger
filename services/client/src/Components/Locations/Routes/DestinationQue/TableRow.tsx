7446f5c213823bfe94f98bd8f2146e57f052781e
ThreeCrown <st3phanadams7@gmail.com>
2024-10-25 23:20:51 -0700

    VER-6 Added List Reorder Functionality

--- services/client/src/Components/Locations/Routes/DestinationQue/TableRow.tsx
+++ services/client/src/Components/Locations/Routes/DestinationQue/TableRow.tsx
@@ -1,71 +1,81 @@
+import { useSoundEffect } from '@Audio/AudioManager';
 import { LocationChip } from '@Common/Components/Chips/LocationChip';
-import { ExpandMoreTwoTone } from '@mui/icons-material';
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
-import React, { MouseEventHandler, RefObject } from 'react';
-import { Float2 } from 'vl-shared/src/math';
+import { ArrowDropDown, ArrowDropUp, ExpandMoreTwoTone } from '@mui/icons-material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import { useAppDispatch } from '@Redux/hooks';
+import { updateDestinations } from '@Redux/Slices/Routes/actions/destination.action';
+import React from 'react';
 import { IDestination } from 'vl-shared/src/schemas/RoutesSchema';
 
 type TableRowProps = {
   destination: IDestination;
+  list: IDestination[];
   distance: string;
   draggable?: boolean;
+  index: number;
   ['data-testid']?: string;
 };
+//VER-6
 
 export const DestinationTableRow: React.FC<TableRowProps> = ({
   destination,
   distance,
+  list: destinations,
+  index: listPosition,
   draggable,
   'data-testid': testid = 'TableRow',
 }) => {
-  const dragging = React.useRef(false);
-  const block: RefObject<HTMLDivElement> = React.useRef<HTMLDivElement>(
-    null as unknown as HTMLDivElement,
+  const dispatch = useAppDispatch();
+  const sound = useSoundEffect();
+
+  const handleReorder = React.useCallback(
+    (index: number, direction: 'up' | 'down') => {
+      // Prevent Moving Top Item Up
+      if (
+        (index === 0 && direction === 'up') ||
+        (index === destinations.length - 1 && direction === 'down')
+      ) {
+        sound.playSound('denied');
+        return;
+      }
+
+      // Define the new Index Value
+      const newIndex = direction === 'up' ? index - 1 : index + 1;
+
+      // Get updatedDestinations Array
+      const updatedDestinations: IDestination[] = destinations.reduce<IDestination[]>(
+        (acc, dest, idx) => {
+          if (idx === index) {
+            // If the Index eq set newIndex & Update Stop Number
+            acc[idx] = { ...dest, stopNumber: newIndex + 1 };
+          } else if (idx === newIndex) {
+            // If the Index eq newIndex moves the Stop
+            acc[idx] = { ...dest, stopNumber: index + 1 };
+          } else {
+            if (dest.stopNumber > index + 1 && dest.stopNumber <= newIndex + 1) {
+              acc[idx] = { ...dest, stopNumber: dest.stopNumber - 1 };
+            } else if (dest.stopNumber < index + 1 && dest.stopNumber >= newIndex + 1) {
+              acc[idx] = { ...dest, stopNumber: dest.stopNumber + 1 };
+            } else {
+              acc[idx] = { ...dest };
+            }
+          }
+          return acc;
+        },
+        [],
+      );
+      dispatch(updateDestinations(updatedDestinations));
+    },
+    [sound, destinations, dispatch],
   );
-  const frameID = React.useRef(0);
-  const last = React.useRef(new Float2());
-  const drag = React.useRef(new Float2());
-
-  const handleMove = (e: MouseEvent) => {
-    if (!dragging.current) return;
-    const cur = new Float2(e.pageX, e.pageY);
-    const delta = last.current.subtract(cur);
-    last.current = cur;
-    drag.current = drag.current.subtract(delta);
-
-    cancelAnimationFrame(frameID.current);
-
-    frameID.current = requestAnimationFrame(() => {
-      const transform = `translate3d(${drag.current.x}px, ${drag.current.y}px, 0)`;
-      if (block.current != null) block.current.style.transform = transform;
-    });
-  };
-
-  const handleMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
-    last.current = new Float2(e.pageX, e.pageY);
-    dragging.current = true;
-  };
-
-  const handleMouseUp = () => {
-    dragging.current = false;
-  };
-
-  React.useEffect(() => {
-    document.addEventListener('mousemove', handleMove);
-    document.addEventListener('mouseup', handleMouseUp);
-    return () => {
-      document.removeEventListener('mousemove', handleMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-    };
-  }, []);
-
   return (
-    <Accordion
-      data-testid={`${testid}__Container`}
-      sx={{ width: '100%' }}
-      ref={block}
-      onMouseDown={handleMouseDown}
-    >
+    <Accordion data-testid={`${testid}__Container`}>
       <AccordionSummary
         // data-testid={`${testid}__Summary`}
         expandIcon={<ExpandMoreTwoTone color="secondary" />}
@@ -85,27 +95,72 @@ export const DestinationTableRow: React.FC<TableRowProps> = ({
             padding: '0 .2em',
           }}
         >
-          <Typography
-            data-testid={`${testid}-Summary__DestinationStop`}
-            variant="body2"
-            sx={{
-              fontWeight: 'bold',
-              color: 'info.main',
+          <div
+            data-testid={`${testid}__Destination_Wrapper`}
+            style={{
               display: 'flex',
               gap: '0.5em',
               alignItems: 'center',
               justifyContent: 'center',
             }}
           >
-            {destination.stopNumber === 0 ? 'Unsorted' : `${destination.stopNumber}.`}
-            <LocationChip
-              data-testid={`${testid}__LocationChip`}
-              locationId={destination.location.id}
+            {draggable && (
+              <div
+                data-testid={`${testid}__Reorder_Wrapper`}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <IconButton
+                  data-testid={`${testid}-Reorder__MoveUp_Button`}
+                  size="small"
+                  sx={{ p: 0 }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleReorder(listPosition, 'up');
+                  }}
+                >
+                  <ArrowDropUp data-testid={`${testid}-Reorder__MoveUp_Icon`} />
+                </IconButton>
+                <IconButton
+                  data-testid={`${testid}-Reorder__MoveDown_Button`}
+                  size="small"
+                  sx={{ p: 0 }}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleReorder(listPosition, 'down');
+                  }}
+                >
+                  <ArrowDropDown data-testid={`${testid}-Reorder__MoveDown_Icon`} />
+                </IconButton>
+              </div>
+            )}
+            <Typography
+              data-testid={`${testid}-Summary__DestinationStop`}
+              variant="body2"
               sx={{
-                maxWidth: '130px',
+                fontWeight: 'bold',
+                color: 'info.main',
+                display: 'flex',
+                gap: '0.5em',
+                alignItems: 'center',
+                justifyContent: 'center',
               }}
-            />
-          </Typography>
+            >
+              {destination.stopNumber === 0 ? 'Unsorted' : `${destination.stopNumber}.`}
+              <LocationChip
+                data-testid={`${testid}__LocationChip`}
+                locationId={destination.location.id}
+                sx={{
+                  maxWidth: '130px',
+                }}
+              />
+            </Typography>
+          </div>
+
           <Typography data-testid={`${testid}__StopReason`} color="info">
             {destination.reason}
           </Typography>
