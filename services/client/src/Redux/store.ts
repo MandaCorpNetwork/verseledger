dbf933874ba65c636d2c0b898e73e3de87206ea7
Ethan Snow <bombitmanbomb@gmail.com>
2024-10-27 19:09:59 -0400

    Tanstack (#741)
    * add Tanstack
    
    * update location typing

--- services/client/src/Redux/store.ts
+++ services/client/src/Redux/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { isDev } from '@Utils/isDev';
+import { createLogger } from 'redux-logger';
 
 import { coreReducer } from './reducers';
 import { updateLocationsMiddleware } from './Slices/Locations/updateLocations.middleware';
@@ -11,11 +12,17 @@ export const setupStore = (preloadState?: Partial<RootState>) =>
     reducer: coreReducer,
     preloadedState: preloadState,
     devTools: isDev(),
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({ serializableCheck: false }).concat(
+    middleware: (getDefaultMiddleware) => {
+      const middleware = getDefaultMiddleware({ serializableCheck: false }).concat(
         updateLocationsMiddleware,
         updateUsersMiddleware,
-      ),
+      );
+      if (isDev()) {
+        const logger = createLogger({ collapsed: true, logErrors: true });
+        middleware.push(logger);
+      }
+      return middleware;
+    },
   });
 export type RootState = ReturnType<typeof coreReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
