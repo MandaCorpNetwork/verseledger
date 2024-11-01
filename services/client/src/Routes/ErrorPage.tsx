2f52cdb07a4efd3f846c2a5f5d21a87e31353d43
Ethan Snow <bombitmanbomb@gmail.com>
2024-11-01 14:07:59 -0400

    update generic Error page

--- services/client/src/Routes/ErrorPage.tsx
+++ services/client/src/Routes/ErrorPage.tsx
@@ -1,77 +1,98 @@
-import Error from '@Assets/media/error.gif';
-import { useSoundEffect } from '@Audio/AudioManager';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { enqueueSnackbar } from 'notistack';
-import React from 'react';
-import { useRouteError } from 'react-router-dom';
+import backgroundvideo from '@Assets/media/NotFoundVideo.webm';
+import DigiDisplay from '@Common/Components/Boxes/DigiDisplay';
+import { Box, Button, Typography } from '@mui/material';
+import { useNavigate, useRouteError } from 'react-router-dom';
 
-const ErrorPage: React.FC = () => {
+const ErrorPage: React.FC<unknown> = () => {
+  const navigate = useNavigate();
   const error = useRouteError() as { error: Error } & Error;
+
   const errorMessage = error?.error?.message ?? error?.message;
-  //? Oops
-  if (errorMessage == `Cannot destructure property 'type' of 'vnode' as it is null.`) {
-    enqueueSnackbar('Encountered known error, Reloading application...', {
-      variant: 'error',
-      autoHideDuration: 5000,
-    });
-    window.location.reload();
-  }
-  const sound = useSoundEffect();
-  React.useEffect(() => {
-    sound.playSound('error');
-  }, [sound]);
   return (
-    <Box style={{ minHeight: '100vh', padding: '2em' }}>
-      <Typography variant="h1" align="center">
-        Whoops
-      </Typography>
-      <Typography align="center" sx={{ mb: '5em' }}>
-        Sorry, an error has occured.
-      </Typography>
-      <Box
-        sx={{
-          position: 'relative',
-          display: 'flex',
-          justifyContent: 'center',
-          mt: '2em',
+    <Box
+      data-testid="ErrorPage__Container"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 'calc(100vh - 64px)',
+        overflow: 'hidden',
+        position: 'relative',
+      }}
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        id="videobg"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          transform: 'scale(1.3)',
+          transformOrigin: 'center center',
         }}
       >
-        <Box
+        <source src={backgroundvideo} />
+      </video>
+      <DigiDisplay sx={{ py: '1em', px: '5em', mt: '5em' }}>
+        <Typography
+          variant="h1"
           sx={{
-            borderTop: '2px solid',
-            borderBottom: '2px solid',
-            borderRadius: '10px',
-            overflow: 'hidden',
-            borderColor: 'primary.main',
-            position: 'relative',
+            color: 'text.secondary',
+            textShadow:
+              '0 0 15px rgba(24,252,252,.4), 0 0 10px rgba(252,252,252,.2), 0 0 3px rgba(0,0,0)',
           }}
         >
-          <img src={Error} alt="Error.gif" />
-          <Box
-            sx={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              bgcolor: 'rgba(0,20,100,.6)',
-              padding: '.5em',
-              borderRight: '2px solid',
-              borderLeft: '2px solid',
-              borderRadius: '5px',
-              borderColor: 'secondary.main',
-              overflow: 'hidden',
-              maxWidth: '100%',
-              textAlign: 'center',
-            }}
-          >
-            <Typography variant="subtitle1">
-              {errorMessage ?? 'Something went Wrong'}
-            </Typography>
-          </Box>
-        </Box>
+          Whoops
+        </Typography>
+        <Typography
+          variant="h2"
+          sx={{
+            color: 'text.secondary',
+            textShadow:
+              '0 0 15px rgba(24,252,252,.4), 0 0 10px rgba(252,252,252,.2), 0 0 3px rgba(0,0,0)',
+            letterSpacing: '3px',
+          }}
+        >
+          Something Went Wrong
+        </Typography>
+      </DigiDisplay>
+      <DigiDisplay sx={{ py: '1em', px: '5em', mb: 'auto', mt: '5em' }}>
+        <Typography variant="body1">{errorMessage}</Typography>
+      </DigiDisplay>
+      <Box sx={{ my: 'auto', width: '20%' }}>
+        <Button
+          size="large"
+          variant="contained"
+          fullWidth
+          color="info"
+          onClick={() => {
+            navigate(-1);
+          }}
+          sx={{
+            py: '15px',
+            fontSize: '1.2em',
+            fontWeight: 'bold',
+            letterSpacing: '5px',
+            boxShadow:
+              '0 4px 8px rgba(0,1,19,.7), 0 6px 12px rgba(0,1,19,.6), inset 0 4px 8px rgba(0,1,19,.8), inset 0 -4px 8px rgba(0,1,19,.4)',
+            transition: 'all .1s ease-in-out',
+            '&:active': {
+              transform: 'translateY(4px)',
+            },
+          }}
+        >
+          Return Home
+        </Button>
       </Box>
     </Box>
   );
 };
+
 export default ErrorPage;
