1126d3965cad6ead837af2aae153aa9a3c67d4df
ThreeCrown <st3phanadams7@gmail.com>
2024-11-05 03:07:01 -0800

    Version Update

--- services/client/src/Common/Definitions/AppListings.tsx
+++ services/client/src/Common/Definitions/AppListings.tsx
@@ -149,7 +149,7 @@ export const useMasterAppList = () => {
         disabled: routing,
         icon: <RouteTwoTone />,
         versionLabel: 'Route Tool',
-        version: '2.0.4',
+        version: '2.0.5',
         severityCode: 8,
       },
       {
