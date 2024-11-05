921b93592065f1962488833c5015871054bb7dfd
ThreeCrown <st3phanadams7@gmail.com>
2024-11-04 16:59:58 -0800

    Added Next Destination Box

---
+++ services/client/src/Components/Locations/Routes/RouteViewer/NextDestination.tsx
@@ -0,0 +1,58 @@
+import { DigiBox } from '@Common/Components/Boxes/DigiBox';
+import DigiDisplay from '@Common/Components/Boxes/DigiDisplay';
+import PopupFormDisplay from '@Common/Components/Boxes/PopupFormDisplay';
+import { LocationChip } from '@Common/Components/Chips/LocationChip';
+import { TextField, Typography } from '@mui/material';
+import { IDestination } from 'vl-shared/src/schemas/RoutesSchema';
+
+import { Task } from './Task';
+
+type NextDestinationProps = {
+  destination: IDestination;
+  distance: string;
+};
+
+export const NextDestination: React.FC<NextDestinationProps> = ({
+  destination,
+  distance,
+}) => {
+  const tasks = destination.tasks.map((task) => task);
+  return (
+    <DigiBox sx={{ p: '0.5em', gap: '1em' }}>
+      <DigiDisplay
+        sx={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '1em',
+          py: '0.2em',
+        }}
+      >
+        <Typography
+          variant="body2"
+          sx={{ position: 'absolute', left: 10, color: 'info.main', fontWeight: 'bold' }}
+        >
+          {destination.stopNumber}.
+        </Typography>
+        <Typography variant="h6">Next Destination</Typography>
+        <LocationChip locationId={destination.location.id} size="medium" />
+      </DigiDisplay>
+      <div style={{ gap: '1em', display: 'flex', padding: '0 0.5em' }}>
+        <TextField size="small" label="Local Time" color="secondary" disabled />
+        <TextField size="small" label="Distance" color="secondary" value={distance} />
+      </div>
+      <div style={{ gap: '0.5em', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ gap: '1em', display: 'flex', padding: '0 0.5em' }}>
+          <TextField size="small" label="SCU Retrieving" color="secondary" disabled />
+          <TextField size="small" label="SCU Unloading" color="secondary" disabled />
+        </div>
+        <TextField size="small" label="Departing Load" color="secondary" disabled />
+      </div>
+      <PopupFormDisplay sx={{ p: '0.5em 0.2em', overflow: 'auto' }}>
+        {tasks.map((task) => {
+          return <Task key={task.id} task={task} destination={destination} />;
+        })}
+      </PopupFormDisplay>
+    </DigiBox>
+  );
+};
--- services/client/src/Components/Locations/Routes/RouteViewer/RouteViewer.tsx
+++ services/client/src/Components/Locations/Routes/RouteViewer/RouteViewer.tsx
@@ -9,6 +9,7 @@ import { IDestination } from 'vl-shared/src/schemas/RoutesSchema';
 import { formatDistance, getMappedLocation, MappedLocation } from '../RouteUtilities';
 import { CheckpointDestination } from './CheckpointDestination';
 import { CurrentDestination } from './CurrentDestination';
+import { NextDestination } from './NextDestination';
 
 type RouteViewerProps = {
   destinations: IDestination[];
@@ -73,8 +74,10 @@ export const RouteViewer: React.FC<RouteViewerProps> = ({
           <Button variant="contained">Add Stop</Button>
         </div>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row', flexGrow: 1 }}>
-        <GlassDisplay sx={{ p: '1em', height: '100%', gap: '1em' }}>
+      <div style={{ display: 'flex', flexDirection: 'row', flexGrow: 1, gap: '1em' }}>
+        <GlassDisplay
+          sx={{ p: '1em', height: '100%', gap: '1em', justifyContent: 'space-around' }}
+        >
           <CurrentDestination destination={currentDestination} />
           {nextIsCheckpoint && nextDestination.tasks.length <= 1 && (
             <CheckpointDestination
@@ -82,118 +85,15 @@ export const RouteViewer: React.FC<RouteViewerProps> = ({
               distance={nextDistance}
             />
           )}
+          {nextIsCheckpoint && nextDestination.tasks.length <= 1 && (
+            <NextDestination destination={followingStop} distance={followingDistance} />
+          )}
+          {(!nextIsCheckpoint || nextDestination.tasks.length > 1) && (
+            <NextDestination destination={nextDestination} distance={nextDistance} />
+          )}
         </GlassDisplay>
-        <GlassDisplay></GlassDisplay>
+        <GlassDisplay>Table Go here</GlassDisplay>
       </div>
     </GlassBox>
   );
 };
-
-// export const RouteViewer: React.FC<RouteViewerProps> = ({ destinations }) => {
-//   return (
-//     <GlassBox
-//       data-testid="RouteTool__RouteViewer_Container"
-//       sx={{ p: '1em', gap: '1em', height: '100%', overflow: 'hidden' }}
-//     >
-//       <Box
-//         data-testid="RouteTool-RouteViewer__Title_Wrapper"
-//         sx={{
-//           display: 'flex',
-//           flexDirection: 'row',
-//           alignItems: 'center',
-//           justifyContent: 'space-between',
-//           gap: '1em',
-//         }}
-//       >
-//         <Typography data-testid="RouteTool-RouteViewer__Title" variant="h5">
-//           Route Viewer
-//         </Typography>
-//         <Button
-//           data-testid="RouteTool-RouteViewer__OpenWidget__Button"
-//           variant="popupButton"
-//           onClick={handleOpenRouteWidget}
-//         >
-//           Open Widget
-//         </Button>
-//         <Button
-//           data-testid="RouteTool-RouteViewer__AddStop__Button"
-//           variant="popupButton"
-//         >
-//           Add Stop
-//         </Button>
-//       </Box>
-//       {destinations && destinations.length > 0 && (
-//         <GlassBox
-//           data-testid="RouteTool-RouteViewer__DestinationViewer_Container"
-//           sx={{
-//             flexGrow: 1,
-//             overflow: 'auto',
-//             p: '.5em',
-//             gap: '1em',
-//           }}
-//         >
-//           {destinations[0].reason === 'Mission' ? (
-//             <CurrentDestination
-//               destination={destinations[0]}
-//               nextDestination={destinations[1] ?? undefined}
-//             />
-//           ) : (
-//             <OtherStop
-//               destination={destinations[0]}
-//               nextDestination={destinations[1] ?? undefined}
-//             />
-//           )}
-//           {destinations.slice(1).map((dest, index) => {
-//             const originalIndex = index + 1; // Adjust for the slice
-//             const nextDestination = destinations[originalIndex + 1] ?? undefined;
-//             return dest.reason === 'Mission' ? (
-//               <NextDestination
-//                 key={dest.id}
-//                 destination={dest}
-//                 nextDestination={nextDestination}
-//               />
-//             ) : (
-//               <OtherStop
-//                 key={dest.id}
-//                 destination={dest}
-//                 nextDestination={nextDestination}
-//               />
-//             );
-//           })}
-//           <Button
-//             data-testid="RouteTool-RouteViewer-DestinationViewer__NextStop_Button"
-//             variant="contained"
-//             color="success"
-//             endIcon={<DoubleArrow />}
-//             disabled={!allowNextStop}
-//             onClick={() => handleDeleteDestination(destinations[0].id)}
-//             sx={{
-//               position: 'sticky',
-//               bottom: 0,
-//               mb: '5px',
-//               mr: '5px',
-//               ml: 'auto',
-//               width: '200px',
-//             }}
-//           >
-//             Next Stop
-//           </Button>
-//         </GlassBox>
-//       )}
-//       {(destinations.length === 0 || destinations == null) && (
-//         <Typography
-//           variant="h4"
-//           sx={{
-//             textAlign: 'center',
-//             width: '100%',
-//             color: 'grey',
-//             textShadow: '0 0 3px rgb(0,0,0), 0 0 10px rgba(0,0,0,.7)',
-//             mt: '5em',
-//           }}
-//         >
-//           No Routes
-//         </Typography>
-//       )}
-//     </GlassBox>
-//   );
-// };
