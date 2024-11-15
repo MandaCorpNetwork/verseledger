fc5ef8a05881d3f5d9d878e1f396551774328144
ThreeCrown <st3phanadams7@gmail.com>
2024-11-15 10:13:42 -0800

    Added Activity Box & Title Bar

--- services/client/src/Components/Orgs/Dash/OrgDash.tsx
+++ services/client/src/Components/Orgs/Dash/OrgDash.tsx
@@ -1,5 +1,50 @@
-import { NoOrgs } from '../NoOrgs';
+import { OrgActivityBox } from './OrgActivityBox';
+import { OrgTitleBar } from './OrgTitleBar';
 
 export const OrgDash: React.FC = () => {
-  return <></>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexGrow: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}
+      >
+        <div
+          data-testid="OrgDashboard__Left_Container"
+          style={{
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div>
+            <OrgTitleBar />
+          </div>
+          <OrgActivityBox />
+        </div>
+        <div
+          data-testid="OrgDashboard__Right_Container"
+          style={{
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'space-between',
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 };
