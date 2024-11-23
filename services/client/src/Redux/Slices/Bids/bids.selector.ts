9751de678c2b9c92fe05b5da8f5494631b64e316
ThreeCrown <st3phanadams7@gmail.com>
2024-11-22 19:14:31 -0800

    Updated Contracts & Bid Selectors

--- services/client/src/Redux/Slices/Bids/bids.selector.ts
+++ services/client/src/Redux/Slices/Bids/bids.selector.ts
@@ -21,8 +21,6 @@ export const selectActiveContractors = createSelector([selectContractBids], (bid
   return activeContractors;
 });
 
-//TODO: Find Bid Selectors
-
 export const selectBidPagination = (state: RootState) => ({
   total: state.bids.pagination.total,
   pages: state.bids.pagination.pages,
