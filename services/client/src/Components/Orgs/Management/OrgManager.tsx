6cfbfcb82780b297a8a6a3e8b9b7cb9263c39c51
ThreeCrown <st3phanadams7@gmail.com>
2024-11-30 14:25:32 -0800

    Started Org Management Page

--- services/client/src/Components/Orgs/Management/OrgManager.tsx
+++ services/client/src/Components/Orgs/Management/OrgManager.tsx
@@ -1,3 +1,25 @@
+import { useAppSelector } from '@Redux/hooks';
+import { selectUserMembershipByOrgId } from '@Redux/Slices/Orgs/orgs.selectors';
+import { useParams } from 'react-router-dom';
+
 export const OrgManager: React.FC = () => {
-  return <></>;
+  const { selectedOrgId } = useParams();
+  const membership = useAppSelector((state) =>
+    selectedOrgId ? selectUserMembershipByOrgId(state, selectedOrgId) : null,
+  );
+  const org = membership ? membership.Org : null;
+  return (
+    <div
+      data-testid="OrgManage__Container"
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      {membership && org && <></>}
+    </div>
+  );
 };
