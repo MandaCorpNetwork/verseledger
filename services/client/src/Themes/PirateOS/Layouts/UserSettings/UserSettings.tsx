0bd7cf2bff2c88c4079d27dd1509592d54abb8fd
ThreeCrown <st3phanadams7@gmail.com>
2024-12-10 08:23:46 -0800

    Define Types & File Structure

---
+++ services/client/src/Themes/PirateOS/Layouts/UserSettings/UserSettings.tsx
@@ -0,0 +1,8 @@
+export const pirateOSUserSettingsLayout: ThemeLayouts = {
+  'UserSettings.DrawerList': {
+    flexDirection: 'row',
+  },
+  'UserSettings.DrawListButton': {
+    textAlign: 'center',
+  },
+};
