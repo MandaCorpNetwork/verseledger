0b87714360962881e18fd5e9ab87aea05db80297
ThreeCrown <st3phanadams7@gmail.com>
2024-12-10 23:31:05 -0800

    VER-149-Upgrading-Core-Components (#1000)
    * Moved FeatureDisplay to it's definition File
    
    * Moved Component Container to definitions files
    
    * Added ComponentDisplay to Style Definitions
    
    * Moved AppDisplay to Definitions Files
    
    * Verse OS Control Panel Theme
    
    * Added ControlPanel Styles for PirateOS

--- services/client/src/Common/Components/Core/Boxes/AppDisplay.tsx
+++ services/client/src/Common/Components/Core/Boxes/AppDisplay.tsx
@@ -1,105 +1,42 @@
-import { logoThemeMap } from '@Common/Definitions/Themes/themeMaps';
-import { Box, styled } from '@mui/material';
+import { Box, styled, SxProps, Theme } from '@mui/material';
+import { generateStyles } from '@Utils/GenerateStyles';
 
-export const AppDisplay = styled(Box)(({ theme }) => [
-  {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
+const defaultStyles: SxProps<Theme> = (theme: Theme) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  height: '100%',
+  flexGrow: 1,
+  margin: 0,
+  padding: 0,
+  overflowY: 'auto',
+  justifyContent: 'center',
+  position: 'relative',
+  zIndex: 0,
+  [theme.breakpoints.up('md')]: {
+    padding: '0.5em',
+    overflowY: 'hidden',
+  },
+  [theme.breakpoints.up('lg')]: {
+    padding: '1em',
+  },
+  '&::before': {
+    position: 'absolute',
+    content: '""',
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    zIndex: -1,
+    idth: '100%',
     height: '100%',
-    flexGrow: 1,
-    margin: 0,
-    padding: 0,
-    overflowY: 'auto',
-    justifyContent: 'center',
-    position: 'relative',
-    zIndex: 0,
-    [theme.breakpoints.up('md')]: {
-      padding: '0.5em',
-      overflowY: 'hidden',
-    },
-    [theme.breakpoints.up('lg')]: {
-      padding: '1em',
-    },
-    '&:before': {
-      position: 'absolute',
-      content: '""',
-      backgroundSize: 'contain',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-      zIndex: -1,
-      idth: '100%',
-      height: '100%',
-      objectFit: 'cover',
-    },
+    objectFit: 'cover',
   },
-  //VERSEOS BASE THEME
-  theme.themeType === 'verseOS' && {
-    '&::-webkit-scrollbar': {
-      width: '5px',
-      height: '5px',
-    },
-    '&::-webkit-scrollbar-track': {
-      background: theme.palette.action.disabled,
-      borderRadius: '10px',
-    },
-    '&::-webkit-scrollbar-thumb': {
-      borderRadius: '20px',
-      background: theme.palette.divider,
-    },
-  },
-  theme.themeType === 'verseOS' &&
-    theme.fidelity === 'high' && {
-      '&:before': {
-        backgroundImage: `url(${logoThemeMap.verseOSGlow})`,
-      },
-    },
-  theme.themeType === 'verseOS' &&
-    theme.fidelity === 'medium' && {
-      '&:before': {
-        backgroundImage: `url(${logoThemeMap.verseOS})`,
-      },
-    },
-  theme.themeType === 'verseOS' &&
-    (theme.fidelity === 'low' || theme.fidelity === 'potato') && {
-      '&:before': {
-        backgroundImage: `url(${logoThemeMap.verseOSPotato})`,
-      },
-    },
-  //PIRATEOS BASE THEME
-  theme.themeType === 'pirateOS' && {
-    '&::-webkit-scrollbar': {
-      width: '2px',
-      height: '2px',
-    },
-    '&::-webkit-scrollbar-track': {
-      background: theme.palette.divider,
-      borderRadius: '2px',
-    },
-    '&::-webkit-scrollbar-thumb': {
-      background: theme.palette.secondary.main,
-    },
-  },
-  theme.themeType === 'pirateOS' &&
-    theme.fidelity === 'high' && {
-      '&:before': {
-        backgroundImage: `url(${logoThemeMap.pirateOSGlow})`,
-      },
-    },
-  theme.themeType === 'pirateOS' &&
-    theme.fidelity === 'medium' && {
-      '&:before': {
-        backgroundImage: `url(${logoThemeMap.pirateOS})`,
-      },
-    },
-  theme.themeType === 'pirateOS' &&
-    (theme.fidelity === 'low' || theme.fidelity === 'potato') && {
-      '&:before': {
-        backgroundImage: `url(${logoThemeMap.pirateOSPotato})`,
-      },
-    },
-]);
+});
+
+export const AppDisplay = styled(Box)(({ theme }) => ({
+  ...generateStyles(theme, defaultStyles(theme), 'AppDisplay'),
+}));
--- services/client/src/Themes/VerseOS/VerseOS.components.tsx
+++ services/client/src/Themes/VerseOS/VerseOS.components.tsx
@@ -1,5 +1,18 @@
-import { verseOSFeatureContainer } from './Styles/FeatureContainer';
+import {
+  verseOSAppDisplay,
+  verseOSComponentContainer,
+  verseOSComponentDisplay,
+  verseOSControlPanel,
+  verseOSFeatureContainer,
+  verseOSFeatureDisplay,
+} from './Styles/CoreBoxes';
 
 export const verseOSComponents: ThemeStyledComponents = {
+  //CORE BOXES
+  AppDisplay: verseOSAppDisplay,
   FeatureContainer: verseOSFeatureContainer,
+  FeatureDisplay: verseOSFeatureDisplay,
+  ComponentContainer: verseOSComponentContainer,
+  ComponentDisplay: verseOSComponentDisplay,
+  ControlPanel: verseOSControlPanel,
 };
