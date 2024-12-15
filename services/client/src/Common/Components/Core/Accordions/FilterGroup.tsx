c15f3eaa13f5b45e487f763db1249a074d359240
ThreeCrown <st3phanadams7@gmail.com>
2024-12-14 17:47:22 -0800

    Updated Notations

--- services/client/src/Common/Components/Core/Accordions/FilterGroup.tsx
+++ services/client/src/Common/Components/Core/Accordions/FilterGroup.tsx
@@ -2,7 +2,6 @@ import { Accordion, type SxProps } from '@mui/material';
 import { styled, type Theme } from '@mui/material/styles';
 import { generateStyles } from '@Utils/GenerateStyles';
 
-//TODO: Attempt Diving into Accordion Styles to correctly Style the different states.
 const defaultStyle: SxProps<Theme> = (_theme: Theme) => ({
   boxShadow: 'none',
   background: 'transparent',
@@ -12,6 +11,12 @@ const defaultStyle: SxProps<Theme> = (_theme: Theme) => ({
   },
 });
 
+/**
+ * @description An Accordion for Displaying Filter Components in a Dropdown to keep the filters Organized and collapsable
+ * ___
+ * TODO:
+ * Attempt Diving into Accordion Styles to correctly Style the different states.
+ */
 export const FilterGroup = styled(Accordion)(({ theme }) => ({
   ...generateStyles(theme, defaultStyle(theme), 'FilterGroup'),
 }));
