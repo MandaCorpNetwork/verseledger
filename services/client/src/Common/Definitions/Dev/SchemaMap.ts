67d2bbe15b1a53028586e7010c12f6dbc669a783
ThreeCrown <st3phanadams7@gmail.com>
2024-12-18 14:37:26 -0800

    Gutted out NonWorking Implementation

--- services/client/src/Common/Definitions/Dev/SchemaMap.ts
+++
@@ -1,9 +0,0 @@
-import { ContractSchema } from 'vl-shared/src/schemas/contracts/ContractSchema';
-import { ContractTimestampedSchema } from 'vl-shared/src/schemas/LocationSchema';
-import { ZodSchema } from 'zod';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const schemaMap: Record<string, ZodSchema<any>> = {
-  ContractTimestamped: ContractTimestampedSchema,
-  Contract: ContractSchema,
-};
