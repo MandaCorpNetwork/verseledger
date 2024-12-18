429dd04b995e1d3b3f5af86da281674b07623c4c
ThreeCrown <st3phanadams7@gmail.com>
2024-12-17 18:12:36 -0800

    updated to zodAny

--- services/client/src/Common/Definitions/Dev/SchemaMap.ts
+++ services/client/src/Common/Definitions/Dev/SchemaMap.ts
@@ -2,7 +2,8 @@ import { ContractSchema } from 'vl-shared/src/schemas/contracts/ContractSchema';
 import { ContractTimestampedSchema } from 'vl-shared/src/schemas/LocationSchema';
 import { z } from 'zod';
 
-export const schemaMap: Record<string, z.ZodSchema<unknown>> = {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const schemaMap: Record<string, z.ZodSchema<any>> = {
   ContractTimestamped: ContractTimestampedSchema,
   Contract: ContractSchema,
 };
