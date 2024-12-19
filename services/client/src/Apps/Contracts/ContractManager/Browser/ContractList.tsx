bb7cfdc3d9ec9c17128053d72ec78a9b34dfd569
ThreeCrown <st3phanadams7@gmail.com>
2024-12-18 23:26:47 -0800

    Creating New Contract List System

---
+++ services/client/src/Apps/Contracts/ContractManager/Browser/ContractList.tsx
@@ -0,0 +1,32 @@
+import { Box } from '@mui/material';
+import { useDynamicTheme } from '@Utils/Hooks/useDynamicTheme';
+import { useMemo } from 'react';
+
+export const ContractList: React.FC = () => {
+  const themeExtend = useDynamicTheme();
+
+  const layout = useMemo(() => {
+    const container = themeExtend.layout('ContractManager.ContractListContainer');
+
+    return { container };
+  }, [themeExtend]);
+
+  return (
+    <Box
+      data-testid="ContractManager-Browser__ContractList_Container"
+      component="area"
+      aria-label="Contract Selection List"
+      id="ContractManager_ContractList_Container"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'auto',
+        alignItems: 'center',
+        flexGrow: 1,
+        position: 'relative',
+        mb: '1em',
+        ...layout.container,
+      }}
+    ></Box>
+  );
+};
--- services/client/src/Themes/VerseOS/VerseOS.styles.tsx
+++ services/client/src/Themes/VerseOS/VerseOS.styles.tsx
@@ -1,9 +1,11 @@
 import type { ThemeStyledComponents } from '@Common/Definitions/Themes/themeTypes';
 
+import { verseOSContractManager } from './Layouts/ContractManager';
 import { verseOSCoreBoxes } from './Styles/CoreBoxes';
 import { verseOSMenus } from './Styles/Menus';
 
 export const verseOSStyles: ThemeStyledComponents = {
   ...verseOSCoreBoxes,
   ...verseOSMenus,
+  ...verseOSContractManager,
 };
