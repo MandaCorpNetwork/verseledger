27be39e466d374d26e336a013854451cd5c194ba
ThreeCrown <st3phanadams7@gmail.com>
2024-12-23 14:57:03 -0800

    Update DataDisplayToggle.tsx

--- services/client/src/Common/Components/Functional/Applcation/Buttons/DataDisplayToggle.tsx
+++ services/client/src/Common/Components/Functional/Applcation/Buttons/DataDisplayToggle.tsx
@@ -1,44 +1,13 @@
-import { DATA_DISPLAY_KEY } from '@Common/Definitions/Dev/IUserSettings';
-import { DataDisplays } from '@Common/Definitions/Settings/DataDisplay';
 import { ToggleButton, ToggleButtonGroup } from '@mui/material';
-import { useUserSettings } from '@Utils/Hooks/useUserSettings';
-import { Logger } from '@Utils/Logger';
 import type React from 'react';
-import { useCallback, useMemo } from 'react';
 
 type DataDisplayToggleProps = {
   pageKey: string;
 };
 
-export const DataDisplayToggle: React.FC<DataDisplayToggleProps> = ({ pageKey }) => {
-  const settings = useUserSettings();
-
-  const position = useMemo(() => {
-    const index = DataDisplays.indexOf(pageKey);
-    if (index === -1) {
-      Logger.warn(`PageKey "${pageKey}" not found in DataDisplays Listings`);
-    }
-    return index;
-  }, [pageKey]);
-
-  const currentSetting = useMemo(
-    () => settings.getSetting(DATA_DISPLAY_KEY, position),
-    [position, settings],
-  );
-
-  const handleToggle = useCallback(
-    (_e: React.MouseEvent<HTMLElement>, value: 0 | 1) => {
-      console.log('Attempting Toggle');
-      if (value !== null && value !== currentSetting) {
-        console.log(`Toggling to ${value}`);
-        settings.updateSession(DATA_DISPLAY_KEY, currentSetting, position, value);
-      }
-    },
-    [currentSetting, position, settings],
-  );
-
+export const DataDisplayToggle: React.FC<DataDisplayToggleProps> = () => {
   return (
-    <ToggleButtonGroup value={currentSetting} exclusive onChange={handleToggle}>
+    <ToggleButtonGroup exclusive>
       <ToggleButton value={0}>Card</ToggleButton>
       <ToggleButton value={1}>Table</ToggleButton>
     </ToggleButtonGroup>
