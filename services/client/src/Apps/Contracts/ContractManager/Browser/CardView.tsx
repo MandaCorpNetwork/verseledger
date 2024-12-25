c24aefbcab6d66f49b2b2354ae1b330d16779ccf
ThreeCrown <st3phanadams7@gmail.com>
2024-12-24 19:02:38 -0800

    Setup Framework for Archetype List Button

--- services/client/src/Apps/Contracts/ContractManager/Browser/CardView.tsx
+++ services/client/src/Apps/Contracts/ContractManager/Browser/CardView.tsx
@@ -1,29 +1,64 @@
+import { useSoundEffect } from '@Audio/AudioManager';
 import { ArchetypeListButton } from '@Common/Components/Functional/Contracts/ArchetypeListButton';
-import { List } from '@mui/material';
+import { IContractArchetype } from '@Common/Definitions/Contracts/ContractTypes';
+import { Collapse, List, useTheme } from '@mui/material';
 import { useAppSelector } from '@Redux/hooks';
 import { selectContracts } from '@Redux/Slices/Contracts/contracts.selectors';
 import { groupContractsByArchetype } from '@Utils/Contracts/ContractTypeUtils';
 import type React from 'react';
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 /**
  * Virtualized List of Contract Cards for the Contract Manager
  */
 export const CardView: React.FC = () => {
+  const theme = useTheme();
+  const sound = useSoundEffect();
   const contracts = useAppSelector(selectContracts);
+
+  const [openLists, setOpenLists] = useState<Set<IContractArchetype>>(new Set());
 
   const groupedContracts = useMemo(
     () => groupContractsByArchetype(contracts),
     [contracts],
   );
+
+  const toggleCollapse = useCallback(
+    (archetype: IContractArchetype) => {
+      sound.playSound('clickMain');
+      setOpenLists((prev) => {
+        const newSet = new Set(prev);
+        if (newSet.has(archetype)) {
+          newSet.delete(archetype);
+        } else {
+          newSet.add(archetype);
+        }
+        return newSet;
+      });
+    },
+    [sound],
+  );
+
   return (
     <List>
       {Object.keys(groupedContracts).map((archetypeKey) => {
         const archetype = groupedContracts[archetypeKey].archetype;
+        const open = openLists.has(archetype.archetype);
         // const archetypeContracts = groupedContracts[archetypeKey].contracts;
         return (
           <>
-            <ArchetypeListButton key={archetype.archetype} archetype={archetype} />
+            <ArchetypeListButton
+              key={archetype.archetype}
+              archetype={archetype}
+              onClick={() => toggleCollapse(archetype.archetype)}
+            />
+            <Collapse in={open} unmountOnExit mountOnEnter>
+              <List>
+                {groupedContracts[archetypeKey].contracts.map((contract) => (
+                  <></>
+                ))}
+              </List>
+            </Collapse>
           </>
         );
       })}
--- services/client/src/Common/Components/Functional/Contracts/ArchetypeListButton.tsx
+++ services/client/src/Common/Components/Functional/Contracts/ArchetypeListButton.tsx
@@ -1,18 +1,107 @@
 import { ContractArchetypeTree } from '@Common/Definitions/Contracts/ContractArchetypes';
-import { ListItemButton, ListItemIcon, ListItemText, SvgIcon } from '@mui/material';
+import type { SvgIconComponent } from '@mui/icons-material';
+import {
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  SvgIcon,
+  type SxProps,
+} from '@mui/material';
+import type { Theme } from '@mui/material/styles';
+import { useDynamicTheme } from '@Utils/Hooks/useDynamicTheme';
 import type React from 'react';
+import { useMemo } from 'react';
 
 type ListButtonProps = {
   archetype: ContractArchetypeTree;
+  'data-testid'?: string;
+  'aria-label'?: string;
+  onClick: () => void;
+  disabled?: boolean;
+  sx?: SxProps<Theme>;
+  count?: number;
+  slotProps?: {
+    itemIcon?: {
+      sx?: SxProps<Theme>;
+    };
+    itemText?: {
+      sx?: SxProps<Theme>;
+    };
+    endIcon?: {
+      sx?: SxProps<Theme>;
+      component?: SvgIconComponent;
+    };
+  };
 };
 
-export const ArchetypeListButton: React.FC<ListButtonProps> = ({ archetype }) => {
+/**
+ * ListItem Button for the Dropdown View of Contracts in the List Format.
+ * ___
+ * TODO:
+ * - Fix Svg Icons being the wrong setup, try to simplify them
+ */
+export const ArchetypeListButton: React.FC<ListButtonProps> = ({
+  archetype,
+  'data-testid': testId = 'ContractList__ArchetypeButton',
+  'aria-label': ariaLabel = 'Button to Expand List of Contracts Grouped by an Archetype',
+  onClick,
+  disabled,
+  sx,
+  count,
+  slotProps,
+}) => {
+  const extendTheme = useDynamicTheme();
+
+  const layout = useMemo(() => {
+    const listButton = extendTheme.layout('Contracts.ArchetypeListButton');
+    const listIcon = extendTheme.layout('Contracts.ArchetypeListIcon');
+    const listText = extendTheme.layout('Contracts.ArchetypeListText');
+
+    const listButtonOverwrite = {
+      ...listButton,
+      ...sx,
+    };
+    const listIconOverwrite = {
+      ...listIcon,
+      ...slotProps?.itemIcon?.sx,
+    };
+    const listTextOverwrite = {
+      ...listText,
+      ...slotProps?.itemText?.sx,
+    };
+
+    return { listButtonOverwrite, listIconOverwrite, listTextOverwrite };
+  }, [extendTheme, slotProps?.itemIcon?.sx, slotProps?.itemText?.sx, sx]);
+
   return (
-    <ListItemButton>
-      <ListItemIcon>
-        <SvgIcon component={archetype.archetypeIcon} />
+    <ListItemButton
+      data-testid={testId}
+      aria-label={ariaLabel}
+      id={testId}
+      onClick={onClick}
+      disabled={disabled}
+      sx={{
+        ...layout.listButtonOverwrite,
+      }}
+    >
+      <ListItemIcon
+        data-testid={`${testId}__Icon_Wrapper`}
+        aria-labelledby={testId}
+        sx={{
+          ...layout.listIconOverwrite,
+        }}
+      >
+        <SvgIcon
+          data-testid={`${testId}__Icon`}
+          aria-labelledby={testId}
+          component={archetype.archetypeIcon}
+        />
       </ListItemIcon>
-      <ListItemText primary={archetype.archetypeLabel} />
+      <ListItemText
+        data-testid={`${testId}__Label`}
+        aria-labelledby={testId}
+        primary={archetype.archetypeLabel}
+      />
     </ListItemButton>
   );
 };
