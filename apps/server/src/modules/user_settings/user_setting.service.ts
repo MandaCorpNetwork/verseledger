de281f121b220c785bf8dae1a9b8f413930486df
Ethan Snow <bombitmanbomb@gmail.com>
2025-11-11 12:48:17 -0500

    add bulk settings behavior

--- apps/server/src/modules/user_settings/user_setting.service.ts
+++ apps/server/src/modules/user_settings/user_setting.service.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm";
 
 import { UserSetting } from "#/entities/user/user_setting.entity";
+import { UserSettingKey } from "#/shared/enum/UserSettingKey";
 
 import { UserSettingsDTO } from "./dto/user_settings.dto";
 
@@ -15,5 +16,31 @@ export class UserSettingService {
       {} as UserSettingsDTO,
     );
     return settings;
+  }
+
+  public objectToSettings(
+    settings: UserSettingsDTO,
+    user_id?: string,
+  ): UserSetting[] {
+    return Object.entries(settings).map(([key, value]) => {
+      const setting = new UserSetting();
+      setting.key = key as UserSettingKey;
+      setting.value = value;
+      setting.user_id = user_id;
+      return setting;
+    });
+  }
+  public async getUserSettings(user_id: string) {
+    const settings = await this.userSettingRepository.find({
+      where: { user_id },
+    });
+    return this.settingsToObj(settings);
+  }
+
+  public async updateUserSettings(user_id: string, settings: UserSettingsDTO) {
+    const userSettings = this.objectToSettings(settings, user_id);
+    return await this.userSettingRepository.upsert(userSettings, {
+      conflictPaths: ["key"],
+    });
   }
 }
